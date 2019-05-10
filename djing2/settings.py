20f614a8d4d7c36f1e0c3e506b7f303e4c4dde41
Dmitry Novikov <nerosketch@gmail.com>
2019-05-11 00:02:04 +0300

    dev

--- djing2/settings.py
+++ djing2/settings.py
@@ -198,16 +198,18 @@ CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_METADATA_CLASS': 'rest_framework.metadata.SimpleMetadata'
+    'DEFAULT_METADATA_CLASS': 'rest_framework.metadata.SimpleMetadata',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 if DEBUG:
-    print('CORS')
     CORS_ORIGIN_ALLOW_ALL = True
     CORS_ALLOW_CREDENTIALS = True
     CORS_ORIGIN_WHITELIST = (
-        'localhost:8080',
+        'http://localhost:8080',
     )
     CORS_ORIGIN_REGEX_WHITELIST = (
-        'localhost:8080',
+        'http://localhost:8080',
     )
