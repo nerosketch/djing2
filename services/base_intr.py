96f4389d6848222110ee69792e6d6d6e6c9880a7
Dmitry Novikov <nerosketch@gmail.com>
2019-05-16 18:43:50 +0300

    add services app

---
+++ services/base_intr.py
@@ -0,0 +1,66 @@
+from abc import ABCMeta, abstractmethod
+from datetime import datetime
+from typing import AnyStr, Optional, Union
+
+
+class ServiceBase(metaclass=ABCMeta):
+    @abstractmethod
+    def calc_cost(self) -> float:
+        """Calculates total cost of payment"""
+        raise NotImplementedError
+
+    @abstractmethod
+    def calc_deadline(self) -> datetime:
+        """Calculate deadline date"""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def description(self) -> AnyStr:
+        """
+        Usage in djing.lib.MyChoicesAdapter for choices fields.
+        :return: human readable description
+        """
+
+    @classmethod
+    def get_description(cls):
+        return cls.description
+
+    @staticmethod
+    def manage_access(customer) -> bool:
+        """Manage for access to customer services"""
+        if not customer.is_active:
+            return False
+        act_srv = customer.active_tariff()
+        if act_srv:
+            return True
+        return False
+
+
+class PeriodicPayCalcBase(metaclass=ABCMeta):
+    @abstractmethod
+    def calc_amount(self, model_object) -> float:
+        """
+        :param model_object: it is a instance of models.PeriodicPay model
+        :return: float: amount for the service
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_next_time_to_pay(self, model_object, last_time_payment: Optional[Union[datetime, None]]) -> datetime:
+        """
+        :param model_object: it is a instance of models.PeriodicPay model
+        :param last_time_payment: May be None if first pay
+        :return: datetime.datetime: time for next pay
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def description(self) -> AnyStr:
+        """Return text description.
+        Uses in djing.lib.MyChoicesAdapter for CHOICES fields"""
+
+    @classmethod
+    def get_description(cls):
+        return cls.description
