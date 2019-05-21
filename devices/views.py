53b6d0e0a3e8fc2074c45d94f7866c7c1d537cb5
Dmitry Novikov <nerosketch@gmail.com>
2019-05-22 01:18:09 +0300

    add search

--- devices/views.py
+++ devices/views.py
@@ -1,3 +1,5 @@
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from djing2.viewsets import DjingModelViewSet
 from devices.models import Device, Port
 from devices.serializers import DeviceModelSerializer, PortModelSerializer
@@ -7,6 +9,8 @@ class DeviceModelViewSet(DjingModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceModelSerializer
     filterset_fields = ('group', 'dev_type', 'status', 'is_noticeable')
+    filter_backends = (SearchFilter, DjangoFilterBackend)
+    search_fields = ('comment', 'ip_address', 'mac_addr')
 
 
 class PortModelViewSet(DjingModelViewSet):
