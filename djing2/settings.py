fa8a3bed10cb516e348d14616e17a2f93ba650dd
Dmitry Novikov <nerosketch@gmail.com>
2019-05-22 13:35:15 +0300

    increase pagination page size to 50

--- djing2/settings.py
+++ djing2/settings.py
@@ -202,7 +202,7 @@ CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 50,
     'DEFAULT_METADATA_CLASS': 'rest_framework.metadata.SimpleMetadata',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
