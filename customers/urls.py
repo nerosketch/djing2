94ac3780c7c9cf0c0255145dbb40101c709af931
Dmitry Novikov <nerosketch@gmail.com>
2019-07-02 13:22:11 +0300

    fix urls

--- customers/urls.py
+++ customers/urls.py
@@ -8,14 +8,14 @@ app_name = 'customers'
 
 router = DefaultRouter()
 router.register('', views.SubscriberModelViewSet)
-router.register('subscriber-service/', views.SubscriberServiceModelViewSet)
-router.register('subscriber-street/', views.SubscriberStreetModelViewSet)
-router.register('subscriber-log/', views.SubscriberLogModelViewSet)
-router.register('passport/', views.PassportInfoModelViewSet)
-router.register('invoices/', views.InvoiceForPaymentModelViewSet)
-router.register('subscriber-raw-password/', views.SubscriberRawPasswordModelViewSet)
-router.register('additional-telephone/', views.AdditionalTelephoneModelViewSet)
-router.register('periodic-pay/', views.PeriodicPayForIdModelViewSet)
+router.register('subscriber-service', views.SubscriberServiceModelViewSet)
+router.register('subscriber-street', views.SubscriberStreetModelViewSet)
+router.register('subscriber-log', views.SubscriberLogModelViewSet)
+router.register('passport', views.PassportInfoModelViewSet)
+router.register('invoices', views.InvoiceForPaymentModelViewSet)
+router.register('subscriber-raw-password', views.SubscriberRawPasswordModelViewSet)
+router.register('additional-telephone', views.AdditionalTelephoneModelViewSet)
+router.register('periodic-pay', views.PeriodicPayForIdModelViewSet)
 
 urlpatterns = [
     path('generate_username/', views.SubscriberModelViewSet.as_view({'get': 'generate_random_username'})),
