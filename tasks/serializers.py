74c170f9512ade0b5757dc053b9681d9c6a17442
Dmitry Novikov <nerosketch@gmail.com>
2019-07-14 17:25:23 +0300

    add fetch tasks

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -13,8 +13,13 @@ class ChangeLogModelSerializer(serializers.ModelSerializer):
 class TaskModelSerializer(serializers.ModelSerializer):
     author_name = serializers.CharField(source='author.get_full_name', read_only=True)
     customer_name = serializers.CharField(source='customer.get_full_name', read_only=True)
+    customer_group = serializers.CharField(source='customer.group.title', read_only=True)
+    customer_street = serializers.CharField(source='customer.street.name', read_only=True)
+    customer_house = serializers.CharField(source='customer.house', read_only=True)
     comment_count = serializers.IntegerField(source='extracomment.count', read_only=True)
     recipients_count = serializers.IntegerField(source='recipients.count', read_only=True)
+    state_str = serializers.CharField(source='get_state_display', read_only=True)
+    mode_str = serializers.CharField(source='get_mode_display', read_only=True)
 
     class Meta:
         model = models.Task
