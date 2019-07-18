22574f4ede48bb586443e151fc1edf7e7562a543
Dmitry Novikov <nerosketch@gmail.com>
2019-07-19 02:50:16 +0300

    make group filter code for unique or empty

--- groupapp/models.py
+++ groupapp/models.py
@@ -1,12 +1,19 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 from django.shortcuts import resolve_url
 from django.db import models
+from rest_framework.exceptions import ValidationError
 from djing2.lib.validators import latinValidator
 
 
 class Group(models.Model):
     title = models.CharField(_('Title'), max_length=127, unique=True)
-    code = models.CharField(_('Tech code'), blank=True, max_length=12, validators=(latinValidator,), unique=True)
+    code = models.CharField(
+        _('Tech code'), blank=True, null=True,
+        default=None, max_length=12,
+        validators=(latinValidator,)
+    )
 
     def get_absolute_url(self):
         return resolve_url('group_app:edit', self.pk)
@@ -19,3 +26,14 @@ class Group(models.Model):
 
     def __str__(self):
         return self.title
+
+
+@receiver(pre_save, sender=Group)
+def group_pre_save(sender, instance, **kwargs):
+    """
+    Tech code must be unique or empty
+    """
+    if not instance.code:
+        return
+    if Group.objects.filter(code=instance.code).exists():
+        raise ValidationError(_('Tech code must be unique'))
