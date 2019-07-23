a41b96bcddcda7b734b524bef5ba23cd671536fc
Dmitry Novikov <nerosketch@gmail.com>
2019-07-23 22:05:06 +0300

    add validation of network field

--- networks/serializers.py
+++ networks/serializers.py
@@ -1,8 +1,22 @@
-from rest_framework.serializers import ModelSerializer
+from ipaddress import ip_network
+
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
 from networks.models import NetworkModel
 
 
-class NetworkModelSerializer(ModelSerializer):
+class NetworkModelSerializer(serializers.ModelSerializer):
+    kind_name = serializers.CharField(source='get_kind_display', read_only=True)
+
+    @staticmethod
+    def validate_network(value):
+        if value is None:
+            raise serializers.ValidationError(_('Network can not be empty'))
+        try:
+            net = ip_network(value)
+            return net.compressed
+        except ValueError as e:
+            raise serializers.ValidationError(e, code='invalid')
 
     class Meta:
         model = NetworkModel
