af0b94086c5933bd30d1193e8507398a32a98c61
Dmitry Novikov <nerosketch@gmail.com>
2019-08-04 15:45:31 +0300

    simple tests

--- customers/tests.py
+++ customers/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
+from rest_framework.test import APITestCase #, APIRequestFactory
+# from customers import models
 
-# Create your tests here.
+
+#fc = APIRequestFactory()
+
+
+class CustomAPITestCase(APITestCase):
+    def get(self, *args, **kwargs):
+        return self.client.get(*args, **kwargs)
+
+    def post(self, *args, **kwargs):
+        return self.client.post(*args, **kwargs)
+
+    def setUp(self):
+        um = get_user_model()
+        um.objects.create_superuser(
+            username='admin',
+            password='admin',
+            telephone='+797812345678'
+        )
+        self.client.login(
+            username='admin',
+            password='admin'
+        )
+
+
+class CustomerServiceTestCase(CustomAPITestCase):
+    def test_direct_create(self):
+        r = self.post('/api/customers/customer-service/')
+        self.assertEqual(r.data, _("Not allowed to direct create Customer service, use 'pick_service' url"))
+        self.assertEqual(r.status_code, 403)
+
+
+class CustomerLogAPITestCase(CustomAPITestCase):
+    def test_direct_create(self):
+        r = self.post('/api/customers/customer-log/')
+        self.assertEqual(r.data, _("Not allowed to direct create Customer log"))
+        self.assertEqual(r.status_code, 403)
