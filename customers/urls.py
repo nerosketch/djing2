a19c825157a7fb42b3d0c5ec46bb50809f3cb7ad
Dmitry Novikov <nerosketch@gmail.com>
2019-08-14 15:03:19 +0300

    split admin views and user views

--- customers/urls.py
+++ customers/urls.py
@@ -1,26 +1,36 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from customers import views
+from customers.views import admin_side, user_side
 
 
 app_name = 'customers'
 
 
 router = DefaultRouter()
-router.register('customer-service', views.CustomerServiceModelViewSet)
-router.register('streets', views.CustomerStreetModelViewSet)
-router.register('customer-log', views.CustomerLogModelViewSet)
-router.register('passport', views.PassportInfoModelViewSet)
-router.register('invoices', views.InvoiceForPaymentModelViewSet)
-router.register('customer-raw-password', views.CustomerRawPasswordModelViewSet)
-router.register('additional-telephone', views.AdditionalTelephoneModelViewSet)
-router.register('periodic-pay', views.PeriodicPayForIdModelViewSet)
-router.register('', views.CustomerModelViewSet)
+
+# Admin Views
+router.register('customer-service', admin_side.CustomerServiceModelViewSet)
+router.register('streets', admin_side.CustomerStreetModelViewSet)
+router.register('customer-log', admin_side.CustomerLogModelViewSet)
+router.register('passport', admin_side.PassportInfoModelViewSet)
+router.register('invoices', admin_side.InvoiceForPaymentModelViewSet)
+router.register('customer-raw-password', admin_side.CustomerRawPasswordModelViewSet)
+router.register('additional-telephone', admin_side.AdditionalTelephoneModelViewSet)
+router.register('periodic-pay', admin_side.PeriodicPayForIdModelViewSet)
+router.register('', admin_side.CustomerModelViewSet)
+
+# User side views
+router.register('users/customer', user_side.CustomersReadOnlyModelViewSet)
+
 
 urlpatterns = [
-    path('generate_username/', views.CustomerModelViewSet.as_view({'get': 'generate_random_username'})),
-    path('generate_password/', views.CustomerModelViewSet.as_view({'get': 'generate_random_password'})),
-    path('attach_group_service/', views.AttachServicesToGroups.as_view()),
-    path('groups/', views.CustomersGroupsListAPIView.as_view()),
+    path('generate_username/', admin_side.CustomerModelViewSet.as_view(actions={
+        'get': 'generate_random_username'
+    })),
+    path('generate_password/', admin_side.CustomerModelViewSet.as_view(actions={
+        'get': 'generate_random_password'
+    })),
+    path('attach_group_service/', admin_side.AttachServicesToGroups.as_view()),
+    path('groups/', admin_side.CustomersGroupsListAPIView.as_view()),
     path('', include(router.urls)),
 ]
