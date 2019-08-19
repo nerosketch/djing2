a027aad698fd6e8697b37ee5fb4f028fd2409689
Dmitry Novikov <nerosketch@gmail.com>
2019-08-19 14:53:05 +0300

    added typing for safe digits

--- djing2/lib/__init__.py
+++ djing2/lib/__init__.py
@@ -5,14 +5,14 @@ from datetime import timedelta
 from collections import Iterator
 
 
-def safe_float(fl):
+def safe_float(fl: float) -> float:
     try:
         return 0.0 if not fl else float(fl)
     except (ValueError, OverflowError):
         return 0.0
 
 
-def safe_int(i):
+def safe_int(i: int) -> int:
     try:
         return 0 if not i else int(i)
     except (ValueError, OverflowError):
