5829f8f8348b91df030614143207ec741edfd1e9
Dmitry Novikov <nerosketch@gmail.com>
2019-08-22 17:20:45 +0300

    maked call builder

--- dials/ami/state_event.py
+++ dials/ami/state_event.py
@@ -1,24 +1,88 @@
+from json import dump
 from panoramisk.message import Message
+
+from .call import DialChannel
+from .helps import safe_float
 
 
 class StateEventDispatcher(object):
     def __init__(self):
-        pass
+        self.calls = {}
 
     def unknown(self, *args, **kwargs):
         pass
 
+    def on_new_channel(self, msg: Message):
+        """
+        Создаём новый канал, его первое появление,
+        добавляем в self.calls новый канал по его Uniqueid.
+        Если его Linkedid пустой или равен Uniqueid то для
+        него ещё нет второго канала, он ещё не создан, т.е.
+        на этот звонок ещё не ответили
+        """
+        uid = safe_float(msg.Uniqueid)
+        if not uid:
+            return
+        channel = DialChannel(uid=uid)
+        linked_id = safe_float(msg.Linkedid)
+        if linked_id and linked_id != uid:
+            channel.linked_id = linked_id
+            channel.linked_dial_channel = self.calls.get(linked_id)
+
+        channel.caller_id_num = msg.CallerIDNum
+        self.calls[uid] = channel
+
+    def on_moh_start(self, msg: Message):
+        print(msg.Uniqueid, '------------- Moh Start -------------', end='\n' * 3)
+
     def on_ringing(self, msg: Message):
-        print('------------- Ringing -------------', end='\n'*3)
+        print(msg.Uniqueid, '------------- Ringing -------------', end='\n'*3)
 
     def on_dialing(self, msg: Message):
-        print('------------- Dialing -------------', end='\n' * 3)
+        print(msg.Uniqueid, '------------- Dialing -------------', end='\n' * 3)
+
+    def on_state_down(self, msg: Message):
+        print(msg.Uniqueid, '------------- State Down -------------', end='\n' * 3)
 
     def on_state_up(self, msg: Message):
-        print('------------- State Up -------------', end='\n' * 3)
+        print(msg.Uniqueid, '------------- State Up -------------', end='\n' * 3)
+
+    def on_queue(self, msg: Message):
+        print(msg.Uniqueid, '------------- Came in queue -------------', end='\n' * 3)
 
     def on_hangup(self, msg: Message):
-        print('------------- Hangup -------------', end='\n' * 3)
+        uid = float(msg.Uniqueid)
+        call_channel = self.calls.get(uid)
+        if call_channel:
+            call_channel.on_hangup()
+            with open('./calls.%f.json' % uid, 'w') as f:
+                dump(call_channel.json(), f, ensure_ascii=False, indent=2)
+            del self.calls[uid]
+        else:
+            print('Warning: dial hangup for uid "%s" not found' % uid)
+        print(msg.Uniqueid, '------------- Hangup -------------', msg, end='\n' * 3)
+
+    def on_dial_begin(self, msg: Message):
+        """Звонок начат, надо соединить 2 канала, вызывающий и отвечающий"""
+        # msg.Uniqueid - id вызывающего канала
+        # msg.DestUniqueid - id вызываемого канала
+        print(msg.Uniqueid, '------------- Dial Begin -------------', msg, end='\n' * 3)
+
+    def on_dial_end(self, msg: Message):
+        print(msg.Uniqueid, '------------- Dial End -------------', msg, end='\n' * 3)
 
     def on_set_monitor_filename(self, msg: Message, val: str):
-        print('------------- Set Monitor Fname -------------', val, end='\n' * 3)
+        uid = float(msg.Uniqueid)
+        call_channel = self.calls.get(uid)
+        if call_channel:
+            call_channel.on_set_monitor_fname(val)
+        print(msg.Uniqueid, '------------- Set Monitor Fname -------------', val, end='\n' * 3)
+
+    def on_set_queuecalls(self, msg: Message, val: str):
+        print(msg.Uniqueid, '------------- Set QUEUECALLS -------------', val, end='\n' * 3)
+
+    def on_set_queuehold_time(self, msg: Message, val: str):
+        print(msg.Uniqueid, '------------- Set QUEUEHOLDTIME -------------', val, end='\n' * 3)
+
+    def on_set_queue_talk_time(self, msg: Message, val: str):
+        print(msg.Uniqueid, '------------- Set QUEUETALKTIME -------------', val, end='\n' * 3)
--- dials/ami_runner.py
+++ dials/ami_runner.py
@@ -2,7 +2,7 @@
 import asyncio
 from panoramisk import Manager
 from panoramisk.message import Message
-from pprint import pprint
+# from pprint import pprint
 
 from ami.helps import safe_int
 from ami.state_event import StateEventDispatcher
@@ -16,26 +16,26 @@ manager = Manager(
 dispatcher = StateEventDispatcher()
 
 
-@manager.register_event('*')
-def callback_all_events(mngr: Manager, msg: Message):
-    print('Ev', msg.Event, end=' ')
-    pprint(msg)
-    print('\n' * 3)
+# @manager.register_event('*')
+# def callback_all_events(mngr: Manager, msg: Message):
+#     print('Ev', msg.Event, end=' ')
+#     pprint(msg)
+#     print('\n' * 3)
 
 
 @manager.register_event('Newchannel')
 def new_channel_event(mngr, msg: Message):
     print('#####NewCh', end=' ')
-    pprint(msg.ChannelState)
-    print('\n'*3)
+    dispatcher.on_new_channel(msg)
 
 
 @manager.register_event('Hangup')
 def hangup_ev(mngr, msg):
-    print('#####Hangup from', end=' ')
-    pprint(msg.CallerIDNum)
+    # print('#####Hangup from', end=' ')
+    # pprint(msg.CallerIDNum)
     dispatcher.on_hangup(msg=msg)
-    print('\n' * 3)
+    print('Calls', dispatcher.calls)
+    # print('\n' * 3)
 
 
 @manager.register_event('NewCallerid')
@@ -53,28 +53,29 @@ def new_state_ev(mngr, msg):
     """
 
     state_map = {
+        0: dispatcher.on_state_down,
         3: dispatcher.on_dialing,
         5: dispatcher.on_ringing,
         6: dispatcher.on_state_up
     }
     state = safe_int(msg.ChannelState)
-    print('State', state, type(state))
+    # print('State', state, type(state))
     if state:
         handler = state_map.get(state, dispatcher.unknown)
         r = handler(msg=msg)
-        print('####New caller from', r, msg.CallerIDNum, msg, end='\n'*3)
+        # print('####New caller from', r, msg.CallerIDNum, msg, end='\n'*3)
 
 
 @manager.register_event('DialBegin')
 def dial_begin_ev(mngr, msg):
     """
-    Звонок отвечен
+    Звонок начат
     :param mngr:
     :param msg:
     :return:
     """
     print('#####Dial Begin')
-    pprint(msg)
+    dispatcher.on_dial_begin(msg)
     print('\n' * 3)
 
 
@@ -87,44 +88,50 @@ def dial_end_ev(mngr, msg):
     :return:
     """
     print('#####Dial End')
-    pprint(msg)
-    print('\n' * 3)
+    dispatcher.on_dial_end(msg)
+    # pprint(msg)
+    # print('\n' * 3)
 
 
 @manager.register_event('VarSet')
 def var_set_ev(mngr, msg):
     var = msg.Variable
     vars_map = {
-        'MIXMONITOR_FILENAME': dispatcher.on_set_monitor_filename
+        'MIXMONITOR_FILENAME': dispatcher.on_set_monitor_filename,
+        'QUEUECALLS': dispatcher.on_set_queuecalls,
+        'QUEUEHOLDTIME': dispatcher.on_set_queuehold_time,
+        'QUEUETALKTIME': dispatcher.on_set_queue_talk_time
     }
-    print('Var', var, type(var))
+    # print('Var', var, type(var))
     handler = vars_map.get(var, dispatcher.unknown)
     handler(msg, msg.Value)
 
 
-@manager.register_event('DongleCallStateChange')
-def dongle_call_state_ev(mngr, msg):
-    """
-    События на симке
-    :param mngr:
-    :param msg:
-    :return:
-    """
-    state = msg.NewState
-    states = ('released', 'active')
-    if state in states:
-        print('##### Dongle Call State')
-        pprint(msg)
+# @manager.register_event('DongleCallStateChange')
+# def dongle_call_state_ev(mngr, msg):
+#     """
+#     События на симке
+#     :param mngr:
+#     :param msg:
+#     :return:
+#     """
+#     state = msg.NewState
+#     states = ('released', 'active')
+#     if state in states:
+#         print('##### Dongle Call State')
+#         pprint(msg)
 
 
 @manager.register_event('Newexten')
 def new_exten_ev(mngr, msg):
-    print('##### New exten')
+    # print('##### New exten')
     app = msg.Application
-    cname = msg.CallerIDName
-    cnum = msg.CallerIDNum
-    uid = msg.Uniqueid
-    pprint(msg)
+    # cname = msg.CallerIDName
+    # cnum = msg.CallerIDNum
+    # uid = msg.Uniqueid
+    # pprint(msg)
+    if app == 'Queue' and 'mainq' in msg.AppData:
+        dispatcher.on_queue(msg)
     """
     Начало звонка
     :param mngr:
@@ -133,24 +140,24 @@ def new_exten_ev(mngr, msg):
     """
 
 
-@manager.register_event('QueueCallerJoin')
-def queue_join_ev(mngr, msg):
-    """
-    Попал в очередь
-    :param mngr:
-    :param msg:
-    :return:
-    """
+# @manager.register_event('QueueCallerJoin')
+# def queue_join_ev(mngr, msg):
+#     """
+#     Попал в очередь
+#     :param mngr:
+#     :param msg:
+#     :return:
+#     """
 
 
-@manager.register_event('AgentCalled')
-def agent_called(mngr, msg):
-    """
-    Начался звонок, идут гудки
-    :param mngr:
-    :param msg:
-    :return:
-    """
+# @manager.register_event('AgentCalled')
+# def agent_called(mngr, msg):
+#     """
+#     Начался звонок, идут гудки
+#     :param mngr:
+#     :param msg:
+#     :return:
+#     """
 
 
 def main():
