bc880640f1eb55a8744d2971434c20bcadd87778
Dmitry Novikov <nerosketch@gmail.com>
2019-08-30 18:22:40 +0300

    maked incoming calls

--- dials/urls.py
+++ dials/urls.py
@@ -8,6 +8,7 @@ app_name = 'dials'
 router = DefaultRouter()
 router.register('ats-devices', views.ATSDeviceModelViewSet)
 router.register('dial-log', views.DialLogModelViewSet)
+router.register('dial-accounts', views.DialAccountModelViewSet)
 
 
 urlpatterns = [
