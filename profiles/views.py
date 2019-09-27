aac3e2ca682c922a1a0b573f78142459e00d8b80
Dmitry Novikov <nerosketch@gmail.com>
2019-09-27 18:40:06 +0300

    maked responsibilities action in profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,6 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from djing2.viewsets import DjingModelViewSet
 from profiles.models import UserProfile, UserProfileLog
 from profiles.serializers import UserProfileSerializer, UserProfileLogSerializer
@@ -7,6 +10,11 @@ class UserProfileViewSet(DjingModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     lookup_field = 'username'
+
+    @action(detail=False, url_path='get_responsibilities/(?P<group_id>\d{1,9})')
+    def get_responsibilities_for_group(self, request, group_id: str):
+        profile_ids = UserProfile.objects.get_profiles_by_group(group_id).values_list('pk')
+        return Response(data=(pi[0] for pi in profile_ids))
 
 
 class UserProfileLogViewSet(DjingModelViewSet):
