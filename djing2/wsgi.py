7ec2d4d02f4b5ca2c8e19bfb28bdbc1644a8fc36
Dmitry Novikov <nerosketch@gmail.com>
2019-10-24 00:29:30 +0300

    remove email config

--- djing2/wsgi.py
+++ djing2/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djing2.settings')
+# os.environ.setdefault('PYTHONUNBUFFERED', '1')
 
 application = get_wsgi_application()
