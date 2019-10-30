e52f9d91009a046dd3401d293364d48f5741b37d
Dmitry Novikov <nerosketch@gmail.com>
2019-10-30 21:00:56 +0300

    fix

--- services/serializers.py
+++ services/serializers.py
@@ -1,16 +1,18 @@
-from rest_framework.serializers import ModelSerializer, IntegerField
+from rest_framework.serializers import ModelSerializer, IntegerField, DateTimeField
 
 from services.models import Service, PeriodicPay
 
 
 class ServiceModelSerializer(ModelSerializer):
     usercount = IntegerField(read_only=True)
+    planned_deadline = DateTimeField(source='calc_deadline_formatted', read_only=True)
 
     class Meta:
         model = Service
         fields = (
             'pk', 'title', 'descr', 'speed_in', 'speed_out',
-            'cost', 'calc_type', 'is_admin', 'usercount'
+            'cost', 'calc_type', 'is_admin', 'usercount',
+            'planned_deadline'
         )
 
 
