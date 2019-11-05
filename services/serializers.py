e5f26b13eaa76ba9822a52f9184000cbd1418f28
Dmitry Novikov <nerosketch@gmail.com>
2019-11-05 13:45:28 +0300

    get service users

--- services/serializers.py
+++ services/serializers.py
@@ -1,24 +1,28 @@
-from rest_framework.serializers import ModelSerializer, IntegerField, DateTimeField
+from rest_framework.serializers import (
+    ModelSerializer, IntegerField,
+    DateTimeField, CharField
+)
 
-from services.models import Service, PeriodicPay
+from services import models
 
 
 class ServiceModelSerializer(ModelSerializer):
     usercount = IntegerField(read_only=True)
     planned_deadline = DateTimeField(source='calc_deadline_formatted', read_only=True)
+    calc_type_name = CharField(source='get_calc_type_display', read_only=True)
 
     class Meta:
-        model = Service
+        model = models.Service
         fields = (
             'pk', 'title', 'descr', 'speed_in', 'speed_out',
             'cost', 'calc_type', 'is_admin', 'usercount',
-            'planned_deadline'
+            'planned_deadline', 'calc_type_name'
         )
 
 
 class PeriodicPayModelSerializer(ModelSerializer):
     class Meta:
-        model = PeriodicPay
+        model = models.PeriodicPay
         fields = (
             'name', 'when_add',
             'calc_type', 'amount'
