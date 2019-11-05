336511adf3a1b113800e17e190dc36bad1a28e2d
Dmitry Novikov <nerosketch@gmail.com>
2019-11-05 16:15:26 +0300

    make

--- services/custom_logic/oneshot.py
+++ services/custom_logic/oneshot.py
@@ -6,11 +6,11 @@ from .base_intr import OneShotBaseService
 class ShotDefault(OneShotBaseService):
     description = _('Default shot pay logic')
 
-    def calc_amount(self, model_object) -> float:
+    def calc_cost(self, model_object, request, customer) -> float:
         return safe_float(model_object.cost)
 
-    def before_pay(self):
+    def before_pay(self, request, customer):
         pass
 
-    def after_pay(self):
+    def after_pay(self, request, customer):
         pass
