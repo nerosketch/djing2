13fb7687b3c9a199ffdf12853986a704f752cac1
Dmitry Novikov <nerosketch@gmail.com>
2019-12-15 01:06:34 +0300

    added memcached cach

--- djing2/viewsets.py
+++ djing2/viewsets.py
@@ -1,8 +1,12 @@
 from django.db import IntegrityError
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.exceptions import AuthenticationFailed
+
 
 from profiles.models import BaseAccount
 from djing2.exceptions import UniqueConstraintIntegrityError
@@ -16,6 +20,12 @@ class DjingModelViewSet(ModelViewSet):
             return super().perform_create(serializer)
         except IntegrityError as e:
             raise UniqueConstraintIntegrityError(str(e))
+
+    # Cache requested url for each user for 4 hours
+    @method_decorator(cache_page(60 * 60 * 4))
+    @method_decorator(vary_on_cookie)
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
 class DjingListAPIView(ListAPIView):
