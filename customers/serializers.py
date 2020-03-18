8a389f2cafcb62b77dc64a81ea40838a501fc72f
Dmitry Novikov <nerosketch@gmail.com>
2020-03-18 18:16:52 +0300

    marked ip_address for remove

--- customers/serializers.py
+++ customers/serializers.py
@@ -123,7 +123,7 @@ class CustomerModelSerializer(QueryFieldsMixin, serializers.ModelSerializer):
         # depth = 1
         fields = (
             'pk', 'username', 'telephone', 'fio',
-            'group', 'group_title', 'balance', 'ip_address', 'description', 'street', 'street_name',
+            'group', 'group_title', 'balance', 'description', 'street', 'street_name',
             'house', 'is_active', 'gateway', 'gateway_title', 'auto_renewal_service',
             'device', 'device_comment', 'dev_port', 'last_connected_service', 'current_service',
             'service_title', 'is_dynamic_ip', 'full_name', 'password', 'raw_password'
