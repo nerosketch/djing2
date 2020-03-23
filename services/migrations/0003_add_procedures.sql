cbddeb7022a3dc89334fbc6acbf689cc06db35b1
Dmitry Novikov <nerosketch@gmail.com>
2020-03-23 18:09:38 +0300

    fetched 'subscriber_dynamic_lease' postgres procedure

--- services/migrations/0003_add_procedures.sql
+++ services/migrations/0003_add_procedures.sql
@@ -1,8 +1,8 @@
----
---- Find customer service credentials by device SWITCH credentials.
---- ATTENTION: If you pass null into v_dev_port, then it returns first available
---- user on the found switch, and he will get service WITHOUT pay
----
+--
+-- Find customer service credentials by device SWITCH credentials.
+-- ATTENTION: If you pass null into v_dev_port, then it returns first available
+-- user on the found switch, and he will get service WITHOUT pay
+--
 DROP FUNCTION IF EXISTS find_customer_service_by_device_switch_credentials(macaddr,smallint);
 CREATE OR REPLACE FUNCTION find_customer_service_by_device_switch_credentials(
   v_dev_mac macaddr,
@@ -32,11 +32,11 @@ BEGIN
 end
 $$;
 
----
---- Find customer service credentials by device ONU credentials(mac address).
---- ATTENTION: If you apply this function to switch or other device with multiple ports,
---- then it returns first available user on the found switch, and he will get service WITHOUT pay
----
+--
+-- Find customer service credentials by device ONU credentials(mac address).
+-- ATTENTION: If you apply this function to switch or other device with multiple ports,
+-- then it returns first available user on the found switch, and he will get service WITHOUT pay
+--
 DROP FUNCTION IF EXISTS find_customer_service_by_device_onu_credentials(macaddr);
 CREATE OR REPLACE FUNCTION find_customer_service_by_device_onu_credentials(
   v_dev_mac macaddr
