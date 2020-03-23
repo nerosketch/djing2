b2471f8d4ed343e73b08881d263c625d3811e46e
Dmitry Novikov <nerosketch@gmail.com>
2020-03-23 19:10:41 +0300

    attach 'fetch_subscriber_dynamic_lease' into model

--- networks/tests/__init__.py
+++ networks/tests/__init__.py
@@ -1,5 +1,6 @@
 from .op82 import *
 from .get_free_ip import *
+from .fetch_subscriber_dynamic_lease import *
 
-
-__all__ = ('Option82TestCase', 'IpPoolTestCase')
+__all__ = ('Option82TestCase', 'IpPoolTestCase',
+           'FetchSubscriberDynamicLeaseTestCase')
