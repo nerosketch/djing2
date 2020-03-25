feb8baa7fa54cd49f3c58c7ffcd9877ca21d1150
Dmitry Novikov <nerosketch@gmail.com>
2020-03-26 00:42:59 +0300

    move serializers to package

---
+++ services/serializers/__init__.py
@@ -0,0 +1,10 @@
+from .service import (ServiceModelSerializer,
+                      PeriodicPayModelSerializer,
+                      OneShotPaySerializer)
+from .radius import RadiusServiceRequestSerializer
+
+
+__all__ = ('ServiceModelSerializer',
+           'PeriodicPayModelSerializer',
+           'OneShotPaySerializer',
+           'RadiusServiceRequestSerializer')
