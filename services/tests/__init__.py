378ad787bc9685d61abbaf1948d15c11d87d577d
Dmitry Novikov <nerosketch@gmail.com>
2020-03-27 17:13:59 +0300

    removed finding customer service by onu device

--- services/tests/__init__.py
+++ services/tests/__init__.py
@@ -1,7 +1,5 @@
-from .get_user_credentials_by_ip import GetUserCredentialsByIpTestCase, \
-    GetUserCredentialsByDeviceOnuCredentialsTestCase
+from .get_user_credentials_by_ip import GetUserCredentialsByIpTestCase
 
 __all__ = (
     'GetUserCredentialsByIpTestCase',
-    'GetUserCredentialsByDeviceOnuCredentialsTestCase'
 )
