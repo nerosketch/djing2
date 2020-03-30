90252e65eecd85535060506a286804a461a94c35
Dmitry Novikov <nerosketch@gmail.com>
2020-03-30 18:26:19 +0300

    commented email config and added RADIUS_SESSION_TIME option

--- djing2/settings.py
+++ djing2/settings.py
@@ -274,5 +274,8 @@ if DEBUG:
 # Default dhcp lease time in seconds
 DHCP_DEFAULT_LEASE_TIME = 86400
 
+# Default radius session time
+RADIUS_SESSION_TIME = 3600
+
 # Address to websocket transmitter
 WS_ADDR = '127.0.0.1:3211'
