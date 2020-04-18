a0f42b2b1dcd8ab876885e3e28a8a846a4caeae1
Dmitry Novikov <nerosketch@gmail.com>
2020-04-18 16:21:09 +0300

    little diz

--- profiles/urls.py
+++ profiles/urls.py
@@ -8,13 +8,13 @@ app_name = 'profiles'
 
 
 router = DefaultRouter()
-router.register('accounts', views.UserProfileViewSet)
 router.register('log', views.UserProfileLogViewSet)
 router.register('current', views.CurrentAuthenticatedProfileROViewSet)
+router.register('', views.UserProfileViewSet)
 
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('location-auth/', views.LocationAuth.as_view()),
-    path('token-auth/', obtain_auth_token)
+    path('token-auth/', obtain_auth_token),
+    path('', include(router.urls))
 ]
