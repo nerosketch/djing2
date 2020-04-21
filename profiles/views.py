626788dbc8704378ad120f4e08964359dbb01125
Dmitry Novikov <nerosketch@gmail.com>
2020-04-22 00:37:26 +0300

    added actions for groups responsibility manipulation

--- profiles/views.py
+++ profiles/views.py
@@ -16,10 +16,25 @@ class UserProfileViewSet(DjingModelViewSet):
     serializer_class = UserProfileSerializer
     lookup_field = 'username'
 
-    @action(detail=False, url_path='get_responsibilities/(?P<group_id>\d{1,9})')
-    def get_responsibilities_for_group(self, request, group_id: str):
+    @action(detail=False, url_path='get_profiles_by_group/(?P<group_id>\d{1,9})')
+    def get_profiles_by_group(self, request, group_id: str):
         profile_ids = UserProfile.objects.get_profiles_by_group(group_id).values_list('pk')
         return Response(data=(pi[0] for pi in profile_ids))
+
+    @action(detail=True, methods=('get',))
+    def get_responsibility_groups(self, request, username=None):
+        profile = self.get_object()
+        group_ids = tuple(g[0] for g in profile.responsibility_groups.all().values_list('pk'))
+        return Response(group_ids)
+
+    @action(detail=True, methods=('put',))
+    def set_responsibility_groups(self, request, username=None):
+        profile = self.get_object()
+
+        checked_groups = (int(gi) for gi in request.data.get('groups'))
+        # profile.responsibility_groups.clear()
+        profile.responsibility_groups.set(checked_groups)
+        return Response()
 
 
 class UserProfileLogViewSet(DjingModelViewSet):
