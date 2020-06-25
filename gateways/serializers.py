ee8dcbb0ff4b06a597ab8fcbf083fb0a2755b273
Dmitry Novikov <nerosketch@gmail.com>
2020-06-25 17:45:24 +0300

    prepeared gateways app

--- gateways/serializers.py
+++ gateways/serializers.py
@@ -1,9 +1,13 @@
+from rest_framework import serializers
 from djing2.lib.mixins import BaseCustomModelSerializer
 from gateways.models import Gateway
 
 
 class GatewayModelSerializer(BaseCustomModelSerializer):
+    gw_type_str = serializers.CharField(source='get_gw_type_display', read_only=True)
+
     class Meta:
         model = Gateway
-        fields = ('pk', 'title', 'ip_address', 'ip_port', 'auth_login',
-                  'auth_passw', 'gw_type', 'is_default', 'enabled')
+        fields = ('id', 'title', 'ip_address', 'ip_port', 'auth_login',
+                  'auth_passw', 'gw_type', 'is_default', 'enabled',
+                  'gw_type_str')
--- gateways/signals.py
+++ gateways/signals.py
@@ -1,14 +1,22 @@
 from django.utils.translation import gettext_lazy as _
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from django.contrib.messages import MessageFailure
 from .models import Gateway
 
 
 @receiver(pre_delete, sender=Gateway)
-def nas_pre_delete(sender, **kwargs):
-    nas = kwargs.get("instance")
+def gw_pre_delete(sender, **kwargs):
+    gw = kwargs.get("instance")
     # check if this gateway is default.
     # You cannot remove default server
-    if nas.is_default:
+    if gw.is_default:
         raise MessageFailure(_('You cannot remove default server'))
+
+
+@receiver(pre_save, sender=Gateway)
+def gw_pre_save(sender, **kwargs):
+    gw = kwargs.get("instance")
+    if gw.is_default:
+        if Gateway.objects.filter(is_default=True).exists():
+            raise MessageFailure(_("You can't have two default gateways"))
