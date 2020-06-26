a8eb08eb729a77299a43739d66f2ff596e0ab078
Dmitry Novikov <nerosketch@gmail.com>
2020-06-26 15:35:47 +0300

    prototype for different gateway implementation through facade

---
+++ gateways/gw_facade/mikrotik_gw.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from django.utils.translation import gettext_lazy as _
+from gateways.gw_facade.base_gw import BaseGateway
+
+
+class MikrotikGateway(BaseGateway):
+    description = _('Mikrotik gateway')
+
+    def send_command_add_customer(self) -> Optional[str]:
+        pass
+
+    def send_command_del_customer(self) -> Optional[str]:
+        pass
+
+    def send_command_sync_customer(self) -> Optional[str]:
+        pass
+
+    def ping_customer(self) -> Optional[str]:
+        pass
