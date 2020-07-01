d5672b64078142394c7ac0214ba9099dabba000f
Dmitry Novikov <nerosketch@gmail.com>
2020-07-01 14:29:53 +0300

    prepare for send signal to gateway

--- gateways/gw_facade/base_gw.py
+++ gateways/gw_facade/base_gw.py
@@ -12,28 +12,28 @@ class BaseGateway(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def send_command_add_customer(self) -> Optional[str]:
+    def send_command_add_customer(self, *args, **kwargs) -> Optional[str]:
         """
         Try to open service for subscriber on gateway
         :return: text representation of error if it happened
         """
 
     @abstractmethod
-    def send_command_del_customer(self) -> Optional[str]:
+    def send_command_del_customer(self, *args, **kwargs) -> Optional[str]:
         """
         Try to close service for subscriber on gateway
         :return: text representation of error if it happened
         """
 
     @abstractmethod
-    def send_command_sync_customer(self) -> Optional[str]:
+    def send_command_sync_customer(self, *args, **kwargs) -> Optional[str]:
         """
         Try to synchronize subscriber service on gateway
         :return: text representation of error if it happened
         """
 
     @abstractmethod
-    def ping_customer(self) -> Optional[str]:
+    def ping_customer(self, *args, **kwargs) -> Optional[str]:
         """
         Try to ping-pong with subscribers device
         :return: text representation of error if it happened
