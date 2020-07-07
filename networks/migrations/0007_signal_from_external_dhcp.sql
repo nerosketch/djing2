2e2eb5afc267ca256672e68872fe776a0e88dc4d
Dmitry Novikov <nerosketch@gmail.com>
2020-07-07 13:08:35 +0300

    maked dhcp event prorotype

---
+++ networks/migrations/0007_signal_from_external_dhcp.sql
@@ -0,0 +1,58 @@
+--
+-- dhcp commit event
+--
+CREATE OR REPLACE FUNCTION dhcp_commit_lease_add_update(v_client_ip inet,
+                                                        v_mac_addr macaddr,
+                                                        v_dev_mac macaddr,
+                                                        v_dev_port smallint)
+  RETURNS RECORD
+  LANGUAGE plpgsql
+AS
+$$
+DECLARE
+  t_customer record;
+  t_pool     record;
+  t_lease    record;
+BEGIN
+  -- find customer by device
+  -- if customer is dynamic then check
+  -- if lease is exist, if lease does not exists
+  -- then assign a new session
+
+  -- find customer by device
+  select *
+         into t_customer
+  from find_customer_by_device_credentials(v_dev_mac, v_dev_port);
+  if not FOUND
+  then
+    raise exception 'Customer with device mac=% not found', v_dev_mac;
+    return null;
+  end if;
+
+  -- Find customer leases
+  select nil.id
+  from networks_ip_leases nil
+  where nil.customer_id = t_customer.baseaccount_ptr_id and
+        nil.ip_address = v_client_ip and
+        nil.is_dynamic
+  limit 1;
+  if FOUND
+  then
+    raise notice 'Ip has already attached';
+    return null;
+  end if;
+
+  -- Create lease for customer.
+  --  Find pool for new lease.
+  select id into t_pool from networks_ip_pool where v_client_ip << network limit 1;
+
+  --  Insert new lease
+  insert into networks_ip_leases (ip_address, pool_id, mac_address, customer_id, is_dynamic)
+      values
+      (v_client_ip, t_pool.id, v_mac_addr, t_customer.baseaccount_ptr_id, true)
+      returning id, ip_address, pool_id, lease_time, mac_address, customer_id, is_dynamic
+      into t_lease;
+
+  return t_lease;
+END;
+$$
--- networks/views/generic.py
+++ networks/views/generic.py
@@ -1,11 +1,14 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from django.utils.translation import gettext_lazy as _
+from django.db.utils import IntegrityError
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
 
 from djing2.viewsets import DjingModelViewSet
+from djing2.lib.mixins import SecureApiView
+from djing2.lib import LogicError, DuplicateEntry
 from networks.models import NetworkIpPool, VlanIf, CustomerIpLeaseModel
 from networks.serializers import (NetworkIpPoolModelSerializer,
                                   VlanIfModelSerializer,
@@ -63,3 +66,56 @@ class CustomerIpLeaseModelViewSet(DjingModelViewSet):
             'text': _('Ping ok') if is_pinged else _('no ping'),
             'status': is_pinged
         })
+
+
+class DhcpLever(SecureApiView):
+    #
+    # Api view for dhcp event
+    #
+    http_method_names = ('get',)
+
+    def get(self, request, format=None):
+        del format
+        data = request.query_params.copy()
+        try:
+            r = self.on_dhcp_event(data)
+            if r is not None:
+                if issubclass(r.__class__, Exception):
+                    return Response({'error': str(r)})
+                return Response({'text': r})
+            return Response({'status': 'ok'})
+        except IntegrityError as e:
+            return Response({
+                'status': str(e).replace('\n', ' ')
+            })
+
+    @staticmethod
+    def on_dhcp_event(data: dict) -> str:
+        """
+        :param data = {
+            'client_ip': ip_address('127.0.0.1'),
+            'client_mac': 'aa:bb:cc:dd:ee:ff',
+            'switch_mac': 'aa:bb:cc:dd:ee:ff',
+            'switch_port': 3,
+            'cmd': 'commit'
+        }"""
+        try:
+            act = data.get('cmd')
+            if act is None:
+                return '"cmd" parameter is missing'
+            client_ip = data.get('client_ip')
+            if client_ip is None:
+                return '"client_ip" parameter is missing'
+            if act == 'commit':
+                return CustomerIpLeaseModel.dhcp_commit_lease_add_update(
+                    client_ip=client_ip, mac_addr=data.get('client_mac'),
+                    dev_mac=data.get('switch_mac'), dev_port=data.get('switch_port')
+                )
+            elif act in ['expiry', 'release']:
+                del_count, del_details = CustomerIpLeaseModel.objects.filter(ip_address=client_ip).delete()
+                return "Removed: %d" % del_count
+            else:
+                return '"cmd" parameter is invalid: %s' % act
+        except (LogicError, DuplicateEntry) as e:
+            print('Error: %s:' % e.__class__.__name__, e)
+            return str(e)
