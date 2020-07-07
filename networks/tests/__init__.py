8ebf98fb4cbaed2e4c3b7bdf39cc0cc3a4a525d9
Dmitry Novikov <nerosketch@gmail.com>
2020-07-07 13:08:35 +0300

    maked dhcp event prorotype

--- networks/tests/__init__.py
+++ networks/tests/__init__.py
@@ -3,9 +3,11 @@ from .get_free_ip import IpPoolTestCase
 from .fetch_subscriber_dynamic_lease import FetchSubscriberDynamicLeaseTestCase
 from .fetch_subscriber_static_lease import FetchSubscriberStaticLeaseTestCase
 from .radius_dhcp import RadiusDHCPRequestTestCase
+from .dhcp_commit_lease_add_update import DhcpCommitLeaseAddUpdateTestCase
 
 
 __all__ = ('Option82TestCase', 'IpPoolTestCase',
            'FetchSubscriberDynamicLeaseTestCase',
            'RadiusDHCPRequestTestCase',
-           'FetchSubscriberStaticLeaseTestCase')
+           'FetchSubscriberStaticLeaseTestCase',
+           'DhcpCommitLeaseAddUpdateTestCase')
