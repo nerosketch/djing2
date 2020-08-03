54652e466587908101c303d50a6e9ec36e059041
Dmitry Novikov <nerosketch@gmail.com>
2020-08-03 16:49:48 +0300

    maked field gateways.customers_count

--- gateways/serializers.py
+++ gateways/serializers.py
@@ -5,9 +5,10 @@ from gateways.models import Gateway
 
 class GatewayModelSerializer(BaseCustomModelSerializer):
     gw_type_str = serializers.CharField(source='get_gw_type_display', read_only=True)
+    customers_count = serializers.IntegerField(source='customer_set.count', read_only=True)
 
     class Meta:
         model = Gateway
         fields = ('id', 'title', 'ip_address', 'ip_port', 'auth_login',
                   'auth_passw', 'gw_type', 'is_default', 'enabled',
-                  'gw_type_str')
+                  'gw_type_str', 'customers_count')
