2a6d6c85e6f1a38a745f37eb858600c6c1ab5662
Dmitry Novikov <nerosketch@gmail.com>
2020-08-10 13:49:11 +0300

    improve arping

--- djing2/__init__.py
+++ djing2/__init__.py
@@ -12,12 +12,11 @@ IP_ADDR_REGEX = (
     '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
 )
 
-ARPING_COMMAND = getattr(settings, 'ARPING_COMMAND', 'arping')
-
 
 def ping(ip_addr: str, count=1, arp=False, interval=0.2) -> bool:
     if re.match(IP_ADDR_REGEX, ip_addr):
         if arp:
+            ARPING_COMMAND = getattr(settings, 'ARPING_COMMAND', 'arping')
             response = os.system(f'{ARPING_COMMAND} -qc{count} -W {interval} {ip_addr} > /dev/null')
         else:
             response = os.system(f"`which ping` -4Anq -i {interval} -c{count} -W1 {ip_addr} > /dev/null")
