ce4f2750b00278c28339577a187308aaed3c33be
Dmitry Novikov <nerosketch@gmail.com>
2020-08-12 14:33:44 +0300

    try to enable permissions

--- customers/views/admin_side.py
+++ customers/views/admin_side.py
@@ -5,7 +5,7 @@ from django.db.models import Count
 from django.db.utils import IntegrityError
 from django.utils.translation import gettext_lazy as _, gettext
 from django_filters.rest_framework import DjangoFilterBackend
-from guardian.shortcuts import get_objects_for_user
+# from guardian.shortcuts import get_objects_for_user
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
@@ -311,16 +311,17 @@ class CustomerModelViewSet(DjingModelViewSet):
 
 class CustomersGroupsListAPIView(DjingListAPIView):
     serializer_class = serializers.CustomerGroupSerializer
-    #filter_backends = (OrderingFilter,)
-    #ordering_fields = ('title', 'usercount')
+    # filter_backends = (OrderingFilter,)
+    # ordering_fields = ('title', 'usercount')
 
     def get_queryset(self):
-        return get_objects_for_user(
-            self.request.user,
-            'groupapp.view_group', klass=Group,
-            use_groups=False,
-            accept_global_perms=False
-        ).annotate(usercount=Count('customer'))
+        return Group.objects.annotate(usercount=Count('customer'))
+        # return get_objects_for_user(
+        #     self.request.user,
+        #     'groupapp.view_group', klass=Group,
+        #     use_groups=False,
+        #     accept_global_perms=False
+        # ).annotate(usercount=Count('customer'))
 
 
 class PassportInfoModelViewSet(DjingModelViewSet):
