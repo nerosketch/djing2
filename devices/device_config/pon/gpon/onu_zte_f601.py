0a8acd84cbc0d7cf8ca009a4bd3cef853b179b48
Dmitry Novikov <nerosketch@gmail.com>
2020-08-24 23:04:21 +0300

    do

--- devices/device_config/pon/gpon/onu_zte_f601.py
+++ devices/device_config/pon/gpon/onu_zte_f601.py
@@ -4,6 +4,7 @@ from .onu_zte_f660 import OnuZTE_F660
 
 class OnuZTE_F601(OnuZTE_F660):
     description = 'Zte ONU F601'
+    ports_len = 1
 
     @staticmethod
     def get_config_types() -> ListDeviceConfigType:
