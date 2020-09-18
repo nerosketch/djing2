9950bfc3be88b4cb4ca704f8fb4406d7b56ef459
Dmitry Novikov <nerosketch@gmail.com>
2020-09-18 12:48:02 +0300

    maked func for fetch related permissions

--- groupapp/views.py
+++ groupapp/views.py
@@ -1,7 +1,11 @@
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
+
 from djing2.viewsets import DjingModelViewSet
 from groupapp.models import Group
 from groupapp.serializers import GroupsSerializer
+from profiles.serializers import PermissionModelSerializer
 
 
 class GroupsModelViewSets(DjingModelViewSet):
@@ -9,3 +13,10 @@ class GroupsModelViewSets(DjingModelViewSet):
     serializer_class = GroupsSerializer
     filter_backends = (OrderingFilter,)
     ordering_fields = 'title',
+
+    @action(detail=False)
+    def get_all_related_perms(self, request):
+        related_perms_qs = Group.objects.get_perms4related_models()
+
+        serializer = PermissionModelSerializer(related_perms_qs, many=True)
+        return Response(serializer.data)
