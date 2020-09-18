9f2dcaf58043fd963c71ebe8070a0d8f130e3d09
Dmitry Novikov <nerosketch@gmail.com>
2020-09-18 13:38:14 +0300

    maked recursive rights assignment for groups

--- groupapp/serializers.py
+++ groupapp/serializers.py
@@ -1,10 +1,31 @@
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.models import Permission, Group as ProfileGroup
+from rest_framework import serializers
 from drf_queryfields import QueryFieldsMixin
 
 from groupapp.models import Group
 
 
-class GroupsSerializer(QueryFieldsMixin, ModelSerializer):
+class GroupsSerializer(QueryFieldsMixin, serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('pk', 'title')
+
+
+class SetRelatedPermsRecursiveSerializer(serializers.Serializer):
+
+    permission_ids = serializers.ListField(
+        child=serializers.PrimaryKeyRelatedField(
+            queryset=Permission.objects.all()
+        ),
+        required=True
+    )
+    profile_group = serializers.PrimaryKeyRelatedField(
+        queryset=ProfileGroup.objects.all(),
+        required=True
+    )
+
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
