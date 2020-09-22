a50e9d9234022f2f27daeba57125831b72e394b4
Dmitry Novikov <nerosketch@gmail.com>
2020-09-22 23:55:57 +0300

    notify about high priority tasks

--- tasks/signals.py
+++ tasks/signals.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext as _
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -6,7 +7,16 @@ from tasks.models import Task
 
 
 @receiver(post_save, sender=Task)
-def task_post_save(sender, **kwargs):
+def task_post_save(sender, instance: Task, created=False, **kwargs):
+    if instance.priority == Task.TASK_PRIORITY_HIGHER:
+        send_data({
+            'eventType': 'updatetask',
+            'text': _('High priority task was created') if created else _('High priority task was updated'),
+            'data': {
+                'recipients': list(instance.recipients.only('pk').values_list('pk', flat=True)),
+            }
+        })
+        return
     send_data({
         'eventType': 'updatetask'
     })
