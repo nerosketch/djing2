f1ba00716d2ddcd36b6ae5f39215ed950ee7eeb3
Dmitry Novikov <nerosketch@gmail.com>
2020-10-14 15:15:30 +0300

    try rights

--- services/views/admin_side.py
+++ services/views/admin_side.py
@@ -1,9 +1,11 @@
 from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
+from guardian.shortcuts import get_objects_for_user
 from rest_framework.filters import OrderingFilter
 
 from djing2.lib.filters import CustomObjectPermissionsFilter
 from djing2.viewsets import DjingModelViewSet
+from groupapp.models import Group
 from services.models import Service, PeriodicPay, OneShotPay
 from services.serializers import (
     ServiceModelSerializer,
@@ -18,6 +20,18 @@ class ServiceModelViewSet(DjingModelViewSet):
     filterset_fields = ('groups',)
     filter_backends = (CustomObjectPermissionsFilter, DjangoFilterBackend, OrderingFilter,)
     ordering_fields = ('title', 'speed_in', 'speed_out', 'cost', 'usercount')
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.is_superuser:
+            return qs
+        # TODO: May optimize
+        grps = get_objects_for_user(
+            user=self.request.user,
+            perms='groupapp.view_group',
+            klass=Group
+        )
+        return qs.filter(groups__in=grps)
 
 
 class PeriodicPayModelViewSet(DjingModelViewSet):
