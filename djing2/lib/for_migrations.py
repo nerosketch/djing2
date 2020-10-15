f66e967f9bbd350304528d927121dfdd6f6ef48d
Dmitry Novikov <nerosketch@gmail.com>
2020-10-15 12:21:02 +0300

    maked sites app

--- djing2/lib/for_migrations.py
+++ djing2/lib/for_migrations.py
@@ -1,5 +1,7 @@
 import os
 
+from django.contrib.sites.management import create_default_site
+
 
 def read_all_file(fname, fl):
     curr_dir = os.path.dirname(os.path.abspath(fl))
@@ -13,9 +15,7 @@ def model2default_site(apps, schema_editor, app_name, model_name):
     model_klass = apps.get_model(app_name, model_name)
     site = Site.objects.all().first()
     if site is None:
-        site = Site.objects.create(
-            domain='example.com',
-            name='example.com'
-        )
+        create_default_site(None, apps=apps)
+        site = Site.objects.all().first()
     for ba in model_klass.objects.all().iterator():
         ba.sites.set([site])
