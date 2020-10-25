bf85f7a2c8e4cc1d22db4ff1599c24f143317dd6
Dmitry Novikov <nerosketch@gmail.com>
2020-10-25 16:21:33 +0300

    mk

--- messenger/models.py
+++ messenger/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from messenger.messenger_implementation import MESSENGER_CHOICES
+from messenger.messenger_implementation import MESSENGER_MAP
 from djing2.models import BaseAbstractModel
 from profiles.models import UserProfile
+
+
+_MESSENGER_CHOICES = ((m_num, m_obj.description) for m_num, m_obj in MESSENGER_MAP.items())
 
 
 class Messenger(BaseAbstractModel):
     title = models.CharField(_('Title'), max_length=64)
     description = models.TextField(_('Description'), null=True, blank=True, default=None)
-    bot_type = models.PositiveSmallIntegerField(_('Bot type'), choices=MESSENGER_CHOICES, blank=True)
+    bot_type = models.PositiveSmallIntegerField(_('Bot type'), choices=_MESSENGER_CHOICES, blank=True)
     slug = models.SlugField(_('Slug'))
     token = models.CharField(_('Bot secret token'), max_length=128)
     avatar = models.ImageField(_('Avatar'), upload_to='messenger_avatar', null=True)
+
+    def inbox_data(self, request):
+        bot_type_class = MESSENGER_MAP.get(self.bot_type)
+        if bot_type_class is None:
+            return
+        bot_type_instance = bot_type_class(
+            model=self,
+            request=request
+        )
+        return bot_type_instance.inbox_data(request.data)
 
     def __str__(self):
         return self.title
