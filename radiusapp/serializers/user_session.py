67277fa416699aed51f912865ec963b614f8c577
Dmitry Novikov <nerosketch@gmail.com>
2020-12-10 17:40:47 +0300

    maked views and serializer

---
+++ radiusapp/serializers/user_session.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from radiusapp.models import UserSession
+
+
+class UserSessionModelSerializer(serializers.ModelSerializer):
+    h_input_octets = serializers.IntegerField(read_only=True)
+    h_output_octets = serializers.IntegerField(read_only=True)
+    h_input_packets = serializers.IntegerField(read_only=True)
+    h_output_packets = serializers.IntegerField(read_only=True)
+
+    def create(self, validated_data):
+        # readonly model
+        pass
+
+    def update(self, instance, validated_data):
+        # readonly model
+        pass
+
+    class Meta:
+        model = UserSession
+        exclude = ['customer']
