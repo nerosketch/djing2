b9de6dccbf3ef8267188e83c3ae59ee2351e70b6
Dmitry Novikov <nerosketch@gmail.com>
2020-12-10 17:20:27 +0300

    maked tests for 'create_or_update_radius_session' sql func

--- radiusapp/migrations/0001_initial.sql
+++ radiusapp/migrations/0001_initial.sql
@@ -4,17 +4,17 @@
 
 
 CREATE OR REPLACE FUNCTION create_or_update_radius_session(
-  v_sess_id   uuid,
-  v_ip_addr   inet,
-  v_dev_mac   macaddr,
-  v_dev_port  smallint,
-  v_sess_time interval,
-  v_uname     varchar(32),
-  v_inp_oct   integer,
-  v_out_oct   integer,
-  v_in_pkt    integer,
-  v_out_pkt   integer,
-  v_is_stop   boolean
+  v_sess_id        uuid,
+  v_ip_addr        inet,
+  v_dev_mac        macaddr,
+  v_dev_port       smallint,
+  v_sess_time_secs integer,
+  v_uname          varchar(32),
+  v_inp_oct        integer,
+  v_out_oct        integer,
+  v_in_pkt         integer,
+  v_out_pkt        integer,
+  v_is_stop        boolean
 )
   RETURNS boolean
 LANGUAGE plpgsql
@@ -27,7 +27,9 @@ BEGIN
   -- if found then update existing session
   -- if act-type is stop then finish session
   update user_session
-  set last_event_time = now(), session_duration = v_sess_time, input_octets = v_inp_oct, output_octets = v_out_oct,
+  set last_event_time = now(), radius_username = v_uname, framed_ip_addr = v_ip_addr,
+    session_duration  = make_interval(secs := v_sess_time_secs), input_octets = v_inp_oct,
+    output_octets     = v_out_oct,
     input_packets     = v_in_pkt, output_packets = v_out_pkt, closed = v_is_stop
   where session_id = v_sess_id;
 
@@ -36,15 +38,30 @@ BEGIN
   if upd_count = 0
   then
     -- if not found then create new session
-    with tcust(b_id) as (
-        select baseaccount_ptr_id
-        from find_customer_by_device_credentials(v_dev_mac, v_dev_port)
-        limit 1
-    )
-    insert into user_session (assign_time, last_event_time, radius_username, framed_ip_addr, session_id, session_duration, closed, customer_id)
-    values
-      (now(), now(), v_uname, v_ip_addr, v_sess_id, 0 :: interval, false, (select b_id
-                                                                           from tcust));
+
+    raise notice 'create new session';
+
+    insert into user_session (assign_time, last_event_time, radius_username,
+                              framed_ip_addr, session_id, session_duration,
+                              input_octets, output_octets, input_packets,
+                              output_packets, closed, customer_id)
+      select
+        now(),
+        now(),
+        v_uname,
+        v_ip_addr,
+        v_sess_id,
+        make_interval(secs := 0),
+        0,
+        0,
+        0,
+        0,
+        false,
+        baseaccount_ptr_id
+      from
+          find_customer_by_device_credentials(v_dev_mac, v_dev_port)
+      limit 1;
+
     GET DIAGNOSTICS upd_count = ROW_COUNT;
     if upd_count > 0
     then
@@ -59,5 +76,6 @@ BEGIN
 END;
 $$;
 
-CREATE UNIQUE INDEX usrses_uid_uni_idx ON user_session (session_id)
-WHERE session_id IS NOT NULL;
+CREATE UNIQUE INDEX usrses_uid_uni_idx
+  ON user_session (session_id)
+  WHERE session_id IS NOT NULL;
