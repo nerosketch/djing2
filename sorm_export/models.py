129173e41775f1e8e8d79329a819bdc78404be5e
Dmitry Novikov <nerosketch@gmail.com>
2020-12-23 18:15:28 +0300

    doing customer serializer

--- sorm_export/models.py
+++ sorm_export/models.py
@@ -7,3 +7,29 @@ class ExportStampModel(models.Model):
 
     class Meta:
         db_table = 'sorm_export_stamp'
+
+
+class CommunicationStandardChoices(models.IntegerChoices):
+    NOT_CONCRETE = 0, _('Not concrete')
+    GSM = 1, 'GSM'
+    CDMA = 2, 'CDMA'
+    PSTN = 3, _('PSTN')  # ТФоП сеть
+    FNDT = 4, _('FNDT')  # стационарные сети передачи данных
+    TMS = 5, _('TMS')
+    MOBILE = 6, _('Mobile')
+    WIFI = 7, 'WIFI'
+    WiMAX = 8, 'WiMAX'
+    PERSONAL_RADIO_CALL = 9, _('Personal radio call')
+    VOIP = 10, 'VOIP'  # сеть передачи голосовой информации посредством сети передачи данных
+
+
+class CustomerTypeChoices(models.IntegerChoices):
+    INDIVIDUAL_ENTITY = 0, _('Individual entity')   # Физическое лицо
+    LEGAL_ENTITY = 1, _('Legal entity')             # Юридическое лицо
+
+
+class CustomerDocumentTypeChoices(models.TextChoices):
+    PASSPORT_RF = _('Passport RF')
+    PASSPORT_USSR = _('Passport USSR')
+    PASSPORT_OTHER_COUNTRY = _('Passport other country')
+    MILITARY_TICKET = _('Military ticket')
