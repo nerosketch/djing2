f62b69f7dab6086615c8de23970c8c03e4daa544
Dmitry Novikov <nerosketch@gmail.com>
2020-12-30 14:06:29 +0300

    connected service to signal

---
+++ sorm_export/tasks/service.py
@@ -0,0 +1,17 @@
+from typing import List
+from uwsgi_tasks import task, TaskExecutor
+from services.models import Service
+from sorm_export.hier_export.service import export_nomenclature
+from sorm_export.tasks.task_export import task_export
+
+
+@task(executir=TaskExecutor.SPOOLER)
+def service_export_task(service_id_list: List[int], event_time=None):
+    services = Service.objects.filter(
+        pk__in=service_id_list
+    )
+    data, fname = export_nomenclature(
+        services=services,
+        event_time=event_time
+    )
+    task_export(data, fname)
