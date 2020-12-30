51e7a13a41492084bf58b9a4031d3c2b94d90b10
Dmitry Novikov <nerosketch@gmail.com>
2020-12-30 14:06:29 +0300

    connected service to signal

--- sorm_export/tasks/customer_service.py
+++ sorm_export/tasks/customer_service.py
@@ -6,11 +6,12 @@ from sorm_export.tasks.task_export import task_export
 
 
 @task(executir=TaskExecutor.SPOOLER)
-def customer_service_export_task(customer_service_id_list: List[int]):
+def customer_service_export_task(customer_service_id_list: List[int], event_time=None):
     cservices = CustomerService.objects.filter(
         pk__in=customer_service_id_list
     )
     data, fname = export_customer_service(
-        cservices=cservices
+        cservices=cservices,
+        event_time=event_time
     )
     task_export(data, fname)
