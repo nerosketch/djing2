222f91f2d68d8fc83af5da3093377a540276e07e
Dmitry Novikov <nerosketch@gmail.com>
2021-01-05 12:33:53 +0300

    maked static ip lease export

---
+++ sorm_export/tasks/networks.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from uwsgi_tasks import task, TaskExecutor
+
+from networks.models import CustomerIpLeaseModel
+from sorm_export.hier_export.networks import export_ip_leases
+from sorm_export.tasks.task_export import task_export
+
+
+@task(executir=TaskExecutor.SPOOLER)
+def export_ip_leases_task(customer_lease_id_list: List[int], event_time=None):
+    leases = CustomerIpLeaseModel.objects.filter(
+        pk__in=customer_lease_id_list
+    )
+    data, fname = export_ip_leases(
+        leases=leases,
+        event_time=event_time
+    )
+    task_export(data, fname)
