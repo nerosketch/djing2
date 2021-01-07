2d777508c892c04afd8419f96ec75a80b738f72d
Dmitry Novikov <nerosketch@gmail.com>
2021-01-07 10:44:17 +0300

    fix export services

--- sorm_export/hier_export/base.py
+++ sorm_export/hier_export/base.py
@@ -24,17 +24,17 @@ def simple_export_decorator(fn):
 
 def iterable_export_decorator(fn):
     @wraps(fn)
-    def _wrapped(qs, event_time=None, *args, **kwargs):
+    def _wrapped(event_time=None, *args, **kwargs):
         if event_time is not None and isinstance(event_time, str):
             event_time = datetime.fromisoformat(event_time)
 
-        serializer_class, gen_fn, qs, fname = fn(qs, event_time=event_time, *args, **kwargs)
-        serializer_class.is_valid(raise_exception=True)
+        serializer_class, gen_fn, qs, fname = fn(event_time=event_time, *args, **kwargs)
 
         res_data = map(gen_fn, qs.iterator())
         ser = serializer_class(
             data=list(res_data), many=True
         )
+        ser.is_valid(raise_exception=True)
 
         return ser.data, fname
     return _wrapped
