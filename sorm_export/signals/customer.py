481307ee4983e23ae3288264f82899e75c730982
Dmitry Novikov <nerosketch@gmail.com>
2021-01-07 13:58:59 +0300

    maked service export to sorm

--- sorm_export/signals/customer.py
+++ sorm_export/signals/customer.py
@@ -9,13 +9,16 @@ from customers.models import (
     CustomerRawPassword, AdditionalTelephone,
     PeriodicPayForId
 )
-from sorm_export.tasks.customer import customer_service_export_task, customer_service_finish_export_task
+from sorm_export.tasks.customer import (
+    customer_service_export_task,
+    customer_service_manual_data_export_task
+)
 
 
 @receiver(post_save, sender=Customer)
 def customer_post_save_signal(sender, instance: Optional[Customer] = None,
-                              created=False, update_fields=(), **kwargs):
-    if 'current_service' in update_fields and instance.current_service:
+                              created=False, update_fields=None, **kwargs):
+    if update_fields is not None and 'current_service' in update_fields and instance.current_service:
         # start service for customer
         customer_service_export_task(
             customer_service_id_list=[instance.current_service.pk],
@@ -29,28 +32,50 @@ def customer_passport_info_post_save_signal(sender, instance: Optional[PassportI
     pass
 
 
-@receiver(post_save, sender=CustomerService)
-def customer_service_changed(sender, instance: Optional[CustomerService] = None,
-                             created=False, **kwargs):
-    if created:
-        # if created then customer also changed,
-        # and customer change signal is also called
-        return
-    customer_service_export_task(
-        customer_service_id_list=[instance.pk],
-        event_time=str(datetime.now())
-    )
+# Called when customer extends his service
+# @receiver(continue_service_signal, sender=CustomerService)
+# def customer_service_autoconnected(sender, instance: CustomerService,
+#                                    old_instance_data: CustomerService,
+#                                    **kwargs):
+#     # auto continue customer service
+#     now = datetime.now()
+#     data = [
+#         {
+#             # old service info
+#             'service_id': old_instance_data['service'],
+#             'idents': old_instance_data['customer'],
+#             'parameter': old_instance_data['service_descr'],
+#             'begin_time': old_instance_data['start_time'],
+#             'end_time': now
+#         },
+#         {
+#             # new service info
+#             'service_id': instance.service.pk,
+#             'idents': instance.customer.pk,
+#             'parameter': instance.service.descr,
+#             'begin_time': instance.start_time,
+#             'end_time': instance.deadline
+#         }
+#     ]
+#     customer_service_manual_data_export_task(
+#         data=data,
+#         event_time=now
+#     )
 
 
 @receiver(pre_delete, sender=CustomerService)
 def customer_service_deleted(sender, instance: CustomerService, **kwargs):
     # customer service end of life
     if instance.customer:
-        customer_service_finish_export_task(
-            customer_service_id=instance.service.pk,
-            customer_id=instance.customer.pk,
-            srv_descr=instance.service.descr,
-            srv_begin_time=instance.start_time,
+        dat = [{
+            'service_id': instance.service.pk,
+            'idents': instance.customer.pk,
+            'parameter': instance.service.descr,
+            'begin_time': instance.start_time,
+            'end_time': datetime.now()
+        }]
+        customer_service_manual_data_export_task(
+            data=dat,
             event_time=str(datetime.now())
         )
 
