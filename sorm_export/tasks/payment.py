92c4301f8e3ff8abad536a24a6cce2b669e7cfaa
Dmitry Novikov <nerosketch@gmail.com>
2021-01-07 10:44:17 +0300

    fix export services

--- sorm_export/tasks/payment.py
+++ sorm_export/tasks/payment.py
@@ -1,13 +1,13 @@
 from typing import List
 
-from uwsgi_tasks import task, TaskExecutor
+from uwsgi_tasks import task
 
 from fin_app.models import AllTimePayLog
 from sorm_export.hier_export.payment import export_customer_unknown_payment
 from sorm_export.tasks.task_export import task_export
 
 
-@task(executir=TaskExecutor.SPOOLER)
+@task
 def export_customer_payment_task(pay_log_id_list: List[int], event_time=None):
     leases = AllTimePayLog.objects.filter(
         pk__in=pay_log_id_list
