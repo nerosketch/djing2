f9228af3135fc5672af8af91de69cd1c33519fd8
Dmitry Novikov <nerosketch@gmail.com>
2021-01-07 10:44:17 +0300

    fix export services

--- sorm_export/tasks/service.py
+++ sorm_export/tasks/service.py
@@ -1,11 +1,11 @@
 from typing import List
-from uwsgi_tasks import task, TaskExecutor
+from uwsgi_tasks import task
 from services.models import Service
 from sorm_export.hier_export.service import export_nomenclature
 from sorm_export.tasks.task_export import task_export
 
 
-@task(executir=TaskExecutor.SPOOLER)
+@task
 def service_export_task(service_id_list: List[int], event_time=None):
     services = Service.objects.filter(
         pk__in=service_id_list
