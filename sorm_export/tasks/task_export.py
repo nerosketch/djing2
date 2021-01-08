43bf98c3795bb5b537485a864b57b2fdf4fac4c2
Dmitry Novikov <nerosketch@gmail.com>
2021-01-08 14:55:05 +0300

    maked export loggings

--- sorm_export/tasks/task_export.py
+++ sorm_export/tasks/task_export.py
@@ -1,6 +1,9 @@
 import csv
+from datetime import datetime
+from ftplib import all_errors
 from io import StringIO
 from sorm_export.ftp_worker.func import send_text_file
+from sorm_export.models import ExportStampModel, ExportStampStatusEnum
 
 
 class _Conv2BinStringIO(StringIO):
@@ -11,11 +14,25 @@ class _Conv2BinStringIO(StringIO):
         return r
 
 
+# make export stamp for logging export
+# end send data to ftp
 def task_export(data, filename: str):
-    csv_buffer = _Conv2BinStringIO()
-    csv_writer = csv.writer(csv_buffer, dialect='unix')
-    for row_data in data:
-        row = (eld for elt, eld in row_data.items())
-        csv_writer.writerow(row)
-    csv_buffer.seek(0)
-    send_text_file(csv_buffer, filename)
+    em = ExportStampModel.objects.create(
+        data=data,
+        export_status=ExportStampStatusEnum.NOT_EXPORTED
+    )
+    try:
+        csv_buffer = _Conv2BinStringIO()
+        csv_writer = csv.writer(csv_buffer, dialect='unix')
+        for row_data in data:
+            row = (eld for elt, eld in row_data.items())
+            csv_writer.writerow(row)
+        csv_buffer.seek(0)
+        send_text_file(csv_buffer, filename)
+        em.attempts_count = 1
+        em.last_attempt_time = datetime.now()
+        em.export_status = ExportStampStatusEnum.SUCCESSFUL
+        em.save(update_fields=['attempts_count', 'last_attempt_time', 'export_status'])
+    except all_errors:
+        em.export_status = ExportStampStatusEnum.FAILED
+        em.save(update_fields=['export_status'])
