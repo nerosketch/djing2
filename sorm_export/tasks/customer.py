f4e9e45d1f463af0f350cda6f3df1546e5ed55b3
Dmitry Novikov <nerosketch@gmail.com>
2021-01-08 17:32:02 +0300

    export customer main telephone

--- sorm_export/tasks/customer.py
+++ sorm_export/tasks/customer.py
@@ -1,6 +1,7 @@
 from typing import List
 from uwsgi_tasks import task
 from customers.models import CustomerService
+from sorm_export.hier_export.customer import export_contact
 from sorm_export.hier_export.service import export_customer_service, export_manual_data_customer_service
 from sorm_export.tasks.task_export import task_export
 
@@ -22,5 +23,14 @@ def customer_service_manual_data_export_task(event_time=None, *args, **kwargs):
     data, fname = export_manual_data_customer_service(
         event_time=event_time,
         *args, **kwargs
+    )
+    task_export(data, fname)
+
+
+@task
+def customer_contact_export_task(customer_tels, event_time=None):
+    data, fname = export_contact(
+        customer_tels=customer_tels,
+        event_time=event_time
     )
     task_export(data, fname)
