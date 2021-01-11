ac4745b97cc5327694d24f8ad3ba7d9849ede598
Dmitry Novikov <nerosketch@gmail.com>
2021-01-11 13:39:32 +0300

    refs

--- traf_stat/on_new_dump.sh
+++ traf_stat/on_new_dump.sh
@@ -6,12 +6,14 @@ then
   exit
 fi
 
-parsetst() {
+longprocdir='./longprocdir'
+
+parsedump() {
   fname="$1"
   echo 'INSERT INTO traf_cache (event_time, ip_addr, octets, packets) VALUES'
-  nfdump -r $fname -O ipkg -Nq -A srcip -o 'fmt:%tsr;%sa;%pkt;%byt' 'src net 10.152.0.0/16 or src net 193.104.145.0/24' | while read -r line; do
+  nfdump -r $fname -O ipkg -Nq -A srcip -o 'fmt:%tsr;%sa;%byt;%pkt' 'src net 10.0.0.0/8 or src net 193.104.145.0/24' | while read -r line; do
     arrIn=(${line//;/ })
-    echo "${c}(to_timestamp(${arrIn[0]}), '${arrIn[1]}', ${arrIn[3]}, ${arrIn[2]})"
+    echo "$c(to_timestamp(${arrIn[0]}),'${arrIn[1]}',${arrIn[2]},${arrIn[3]})"
     c=','
   done
   echo "ON CONFLICT (customer_id, ip_addr) DO UPDATE SET
@@ -20,5 +22,7 @@ parsetst() {
     packets = EXCLUDED.packets;"
 }
 
-#parsetst $1
-psql postgresql://postgres:NOT_A_PASSWORD_ACTUALLY@127.0.0.1/djing2 -c "$(parsetst $1)"
+#parsedump $1
+psql postgresql://postgres:NOT_A_PASSWORD_ACTUALLY@127.0.0.1/djing2 -c "$(parsedump $1)"
+
+mv $1 longprocdir
