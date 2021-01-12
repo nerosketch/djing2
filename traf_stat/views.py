07c1c8e80e32a31f7963afc9323486b5d93119c3
Dmitry Novikov <nerosketch@gmail.com>
2021-01-12 17:54:02 +0300

    fix

--- traf_stat/views.py
+++ traf_stat/views.py
@@ -1,3 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+
 from traf_stat.models import TrafficArchiveModel, TrafficCache
 from traf_stat.serializers import TrafficCacheModelSerializer, TrafficArchiveModelSerializer
 from djing2.viewsets import DjingModelViewSet
@@ -6,9 +9,11 @@ from djing2.viewsets import DjingModelViewSet
 class TrafficCacheViewSet(DjingModelViewSet):
     queryset = TrafficCache.objects.all()
     serializer_class = TrafficCacheModelSerializer
+    filterset_fields = ['event_time', 'customer']
 
 
 class TrafficArchiveViewSet(DjingModelViewSet):
     queryset = TrafficArchiveModel.objects.all()
     serializer_class = TrafficArchiveModelSerializer
-    filterset_fields = ['event_time']
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['event_time', 'customer']
