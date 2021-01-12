e98062957f177214363e468829c3f9e6ca053703
Dmitry Novikov <nerosketch@gmail.com>
2021-01-12 16:09:37 +0300

    maked view and serializer for traffic models

--- traf_stat/views.py
+++ traf_stat/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from traf_stat.models import TrafficArchiveModel, TrafficCache
+from traf_stat.serializers import TrafficCacheModelSerializer, TrafficArchiveModelSerializer
+from djing2.viewsets import DjingModelViewSet
 
-# Create your views here.
+
+class TrafficCacheViewSet(DjingModelViewSet):
+    queryset = TrafficCache.objects.all()
+    serializer_class = TrafficCacheModelSerializer
+
+
+class TrafficArchiveViewSet(DjingModelViewSet):
+    queryset = TrafficArchiveModel.objects.all()
+    serializer_class = TrafficArchiveModelSerializer
+    filterset_fields = ['event_time']
