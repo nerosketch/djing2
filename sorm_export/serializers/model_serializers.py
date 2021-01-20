8c5812d8637c5c61b87d63d354f67b8f902db39a
Dmitry Novikov <nerosketch@gmail.com>
2021-01-20 18:16:16 +0300

    ouif

--- sorm_export/serializers/model_serializers.py
+++ sorm_export/serializers/model_serializers.py
@@ -3,6 +3,9 @@ from sorm_export import models
 
 
 class FiasRecursiveAddressModelSerializer(serializers.ModelSerializer):
+    ao_level_name = serializers.CharField(source='get_ao_level_display', read_only=True)
+    ao_type_name = serializers.CharField(source='get_ao_type_display', read_only=True)
+
     class Meta:
         model = models.FiasRecursiveAddressModel
         fields = '__all__'
