14ff0441fd1dc79a66c9fb5acbe96bb4caea1886
Dmitry Novikov <nerosketch@gmail.com>
2021-01-20 16:09:01 +0300

    added 'get_parent' action

--- sorm_export/views.py
+++ sorm_export/views.py
@@ -1,3 +1,6 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from djing2.viewsets import DjingModelViewSet
 from sorm_export import models
 from sorm_export.serializers import model_serializers as serializers
@@ -6,3 +9,12 @@ from sorm_export.serializers import model_serializers as serializers
 class FiasRecursiveAddressModelViewSet(DjingModelViewSet):
     queryset = models.FiasRecursiveAddressModel.objects.all()
     serializer_class = serializers.FiasRecursiveAddressModelSerializer
+
+    @action(methods=['get'], detail=True)
+    def get_parent(self, request, pk=None):
+        obj = self.get_object()
+        parent = obj.parent_ao
+        if not parent:
+            return Response()
+        serializer = self.get_serializer(obj)
+        return Response(serializer.data)
