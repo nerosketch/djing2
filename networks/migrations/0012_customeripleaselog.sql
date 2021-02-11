c082e0965c0c84a90b03f16e1369ad332980b18c
Dmitry Novikov <nerosketch@gmail.com>
2021-02-11 16:29:17 +0300

    maked customer ip lease logging

---
+++ networks/migrations/0012_customeripleaselog.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION networks_ip_lease_log_fn()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  insert into networks_ip_lease_log(customer_id, ip_address, lease_time, last_update, mac_address, is_dynamic, event_time)
+  values (NEW.customer_id, NEW.ip_address, NEW.lease_time, NEW.last_update, NEW.mac_address, NEW.is_dynamic, now());
+  RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
+
+
+CREATE TRIGGER networks_ip_lease_log_trigger
+  AFTER INSERT OR UPDATE
+  ON networks_ip_leases
+  FOR EACH ROW
+EXECUTE PROCEDURE networks_ip_lease_log_fn();
