bd6e7d3509032f7f8839d545389435e0e71ad23e
Dmitry Novikov <nerosketch@gmail.com>
2021-02-15 14:49:22 +0300

    new huawei type

--- devices/device_config/switch/__init__.py
+++ devices/device_config/switch/__init__.py
@@ -3,10 +3,10 @@ from .dlink import (
     DlinkDGS_1100_06MESwitchInterface, DlinkDGS_3627GSwitchInterface
 )
 from .eltex import EltexSwitch
-from .huawei import HuaweiS2300
+from .huawei import HuaweiS2300, HuaweiS5300_10P_LI_AC
 
 __all__ = (
     'DlinkDGS1100_10ME', 'DlinkDGS_3120_24SCSwitchInterface',
     'DlinkDGS_1100_06MESwitchInterface', 'DlinkDGS_3627GSwitchInterface',
-    'EltexSwitch', 'HuaweiS2300'
+    'EltexSwitch', 'HuaweiS2300', 'HuaweiS5300_10P_LI_AC'
 )
