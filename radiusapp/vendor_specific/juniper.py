07026f1786f9c66e16dc4cdac50bfbdc1ddfffac
Dmitry Novikov <nerosketch@gmail.com>
2021-02-17 14:23:15 +0300

    moved vendor specific code into other module

---
+++ radiusapp/vendor_specific/juniper.py
@@ -0,0 +1,10 @@
+from radiusapp.vendors import IVendorSpecific
+
+
+class JuniperVendorSpecific(IVendorSpecific):
+    vendor = 'juniper'
+
+    def parse_option82(self, data):
+        aget_remote_id = self.get_rad_val(data, 'ADSL-Agent-Remote-Id')
+        aget_circ_id = self.get_rad_val(data, 'ADSL-Agent-Circuit-Id')
+        return aget_remote_id, aget_circ_id
