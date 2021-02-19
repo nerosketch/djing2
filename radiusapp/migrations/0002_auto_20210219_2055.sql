c0090f5070c20a7c56195a4d05497ff512278761
Dmitry Novikov <nerosketch@gmail.com>
2021-02-19 21:35:46 +0300

    changed field len for uname field

--- radiusapp/migrations/0002_auto_20210219_2055.sql
+++ radiusapp/migrations/0002_auto_20210219_2055.sql
@@ -103,3 +103,78 @@ BEGIN
   return null;
 END;
 $$;
+
+
+
+--
+-- Creating or updating radius session during accounting.
+--
+
+DROP FUNCTION create_or_update_radius_session(
+  uuid, inet, macaddr, smallint, integer,
+  varchar(32), integer, integer, integer,
+  integer, boolean);
+CREATE OR REPLACE FUNCTION create_or_update_radius_session(
+  v_sess_id        uuid,
+  v_ip_addr        inet,
+  v_dev_mac        macaddr,
+  v_dev_port       smallint,
+  v_sess_time_secs integer,
+  v_uname          character varying,
+  v_inp_oct        integer,
+  v_out_oct        integer,
+  v_in_pkt         integer,
+  v_out_pkt        integer,
+  v_is_stop        boolean
+)
+  RETURNS boolean
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  upd_count integer;
+BEGIN
+
+  -- find existing session by session id
+  -- if found then update existing session
+  -- if act-type is stop then finish session
+  update user_session
+  set last_event_time = now(), radius_username = v_uname, framed_ip_addr = v_ip_addr,
+    session_duration  = make_interval(secs := v_sess_time_secs), input_octets = v_inp_oct,
+    output_octets     = v_out_oct,
+    input_packets     = v_in_pkt, output_packets = v_out_pkt, closed = v_is_stop
+  where session_id = v_sess_id;
+
+  GET DIAGNOSTICS upd_count = ROW_COUNT;
+
+  if upd_count = 0
+  then
+    -- if not found then create new session
+
+    raise notice 'create new session';
+
+    insert into user_session (assign_time, last_event_time, radius_username,
+                              framed_ip_addr, session_id, session_duration,
+                              input_octets, output_octets, input_packets,
+                              output_packets, closed, customer_id)
+      select
+        now(),
+        now(),
+        v_uname,
+        v_ip_addr,
+        v_sess_id,
+        make_interval(secs := 0),
+        0, 0, 0, 0,
+        false,
+        baseaccount_ptr_id
+      from
+          find_customer_by_device_credentials(v_dev_mac, v_dev_port)
+      limit 1;
+
+    GET DIAGNOSTICS upd_count = ROW_COUNT;
+    return upd_count > 0;
+  end if;
+
+  return false;
+
+END;
+$$;
