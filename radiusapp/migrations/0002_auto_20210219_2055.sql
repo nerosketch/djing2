8468c37e7a6571f8adc9cc1781bede7fefeee30f
Dmitry Novikov <nerosketch@gmail.com>
2021-02-23 01:41:12 +0300

    fx

--- radiusapp/migrations/0002_auto_20210219_2055.sql
+++ radiusapp/migrations/0002_auto_20210219_2055.sql
@@ -61,16 +61,19 @@ BEGIN
 
   -- find pools for customer.
   -- fetched all pools that is available in customers group.
-  for t_net in select nipg.networkippool_id
-               from networks_ippool_groups nipg
-               where nipg.group_id = v_customer_group
+  for t_net in select nip.id
+               from networks_ip_pool nip
+                 left join networks_ippool_groups nipg on nipg.networkippool_id = nip.id
+               where (v_customer_group is null or nipg.group_id = v_customer_group)
+                 and nip.kind = v_pool_kind
+    group by nip.id
     loop
 
       -- Find new lease
       select *
              into t_ip
       from find_new_ip_pool_lease(
-        t_net.networkippool_id,
+        t_net.id::integer,
         v_is_dynamic, v_vid,
         v_pool_kind
       );
@@ -81,7 +84,7 @@ BEGIN
         -- lease found, attach to customer and return it
         insert into networks_ip_leases (ip_address, pool_id, mac_address, customer_id, is_dynamic, last_update)
         values
-        (t_ip, t_net.networkippool_id, v_mac_addr, v_customer_id, v_is_dynamic, now())
+        (t_ip, t_net.id, v_mac_addr, v_customer_id, v_is_dynamic, now())
         returning id, ip_address, pool_id, lease_time, mac_address, customer_id, is_dynamic, true
           into t_lease;
         return t_lease;
