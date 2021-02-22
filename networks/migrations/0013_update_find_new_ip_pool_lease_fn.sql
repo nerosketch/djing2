090bab7b312dbdbb284a9e2519fd5b16a70155d0
Dmitry Novikov <nerosketch@gmail.com>
2021-02-23 01:56:25 +0300

    added new test and fixed ip lease logging

--- networks/migrations/0013_update_find_new_ip_pool_lease_fn.sql
+++ networks/migrations/0013_update_find_new_ip_pool_lease_fn.sql
@@ -120,3 +120,17 @@ BEGIN
   return null;
 END
 $$;
+
+
+CREATE OR REPLACE FUNCTION networks_ip_lease_log_fn()
+  RETURNS TRIGGER AS
+$$
+BEGIN
+  if NEW.customer_id is not null then
+    insert into networks_ip_lease_log(customer_id, ip_address, lease_time, last_update, mac_address, is_dynamic, event_time)
+    values (NEW.customer_id, NEW.ip_address, NEW.lease_time, NEW.last_update, NEW.mac_address, NEW.is_dynamic, now());
+  end if;
+  RETURN NEW;
+END
+$$
+LANGUAGE plpgsql;
