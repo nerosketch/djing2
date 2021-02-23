2f8e3d069f52a022a91e5489417d76ade2f4d296
Dmitry Novikov <nerosketch@gmail.com>
2021-02-23 12:27:02 +0300

    fin func

--- radiusapp/serializers/user_session.py
+++ radiusapp/serializers/user_session.py
@@ -8,6 +8,9 @@ class CustomerRadiusSessionModelSerializer(serializers.ModelSerializer):
     h_output_octets = serializers.CharField(read_only=True)
     h_input_packets = serializers.CharField(read_only=True)
     h_output_packets = serializers.CharField(read_only=True)
+    ip_lease_ip = serializers.IPAddressField(source='ip_lease.ip_address', read_only=True)
+    ip_lease_mac = serializers.IPAddressField(source='ip_lease.mac_address', read_only=True)
+    is_guest_session = serializers.BooleanField(read_only=True)
 
     def create(self, validated_data):
         # readonly model
