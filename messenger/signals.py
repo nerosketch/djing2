1981e1d901c7279f0e9a9a60113201dd833c14ee
Dmitry Novikov <nerosketch@gmail.com>
2021-03-14 20:14:33 +0300

    Raises exception 'ImproperlyConfigured' when depended application is not installed

--- messenger/signals.py
+++ messenger/signals.py
@@ -2,15 +2,13 @@ from typing import List
 
 from django.dispatch import receiver
 
+from djing2.lib.custom_signals import notification_signal
 
 try:
-    from devices.custom_signals import device_monitoring_event_signal
-    from devices.models import Device
     from messenger.tasks import multicast_viber_notify
 
-    @receiver(device_monitoring_event_signal, sender=Device,
-              dispatch_uid='dev_monitoring_viber_ev_unique6%487*@')
-    def on_device_monitoring_event(sender, instance: Device, recipients: List[int], text: str, **kwargs):
+    @receiver(notification_signal, dispatch_uid='dev_monitoring_viber_ev_unique6%487*@')
+    def on_device_monitoring_event(sender, instance, recipients: List[int], text: str, **kwargs):
         multicast_viber_notify(
             messenger_id=None,
             account_id_list=recipients,
