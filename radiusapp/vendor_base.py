926ce487e282dae6469cab622dc3c03db92efcb9
Dmitry Novikov <nerosketch@gmail.com>
2021-03-15 12:25:34 +0300

    maked accounting state

--- radiusapp/vendor_base.py
+++ radiusapp/vendor_base.py
@@ -1,8 +1,15 @@
 import abc
-from typing import Optional, Tuple
+import enum
+from typing import Optional, Tuple, Union
 
 from customers.models import CustomerService, Customer
 from radiusapp.models import CustomerRadiusSession, FetchSubscriberLeaseResponse
+
+
+class AcctStatusType(enum.IntEnum):
+    START = 1
+    STOP = 2
+    UPDATE = 3
 
 
 class IVendorSpecific(abc.ABC):
@@ -18,6 +25,15 @@ class IVendorSpecific(abc.ABC):
             k = k.get('value')
             if k:
                 return k[0]
+
+    @staticmethod
+    def get_acct_rad_val(data, v, default=None) -> Optional[Union[str, int]]:
+        attr = data.get(v)
+        if isinstance(attr, (list, tuple)):
+            return attr[0]
+        if attr:
+            return attr
+        return default
 
     @abc.abstractmethod
     def parse_option82(self, data) -> Optional[Tuple[str, str]]:
@@ -47,3 +63,13 @@ class IVendorSpecific(abc.ABC):
                                   customer: Customer,
                                   request_data) -> dict:
         raise NotImplementedError
+
+    def get_acct_status_type(self, request) -> AcctStatusType:
+        dat = request.data
+        r_map = {
+            'Start': AcctStatusType.START,
+            'Stop': AcctStatusType.STOP,
+            'Interim-Update': AcctStatusType.UPDATE
+        }
+        act_type = self.get_acct_rad_val(dat, 'Acct-Status-Type')
+        return r_map.get(act_type)
