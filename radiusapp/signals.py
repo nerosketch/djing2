8ce00bb973c02de70f42774fdda7b31b867543de
Dmitry Novikov <nerosketch@gmail.com>
2021-03-15 18:02:53 +0300

    upd

--- radiusapp/signals.py
+++ radiusapp/signals.py
@@ -1,20 +1,21 @@
 from django.dispatch.dispatcher import receiver
 from django.db.models.signals import pre_delete
-from django.utils.translation import gettext_lazy as _
-from rest_framework.exceptions import APIException
-from rest_framework import status
+# from django.utils.translation import gettext_lazy as _
+# from rest_framework.exceptions import APIException
+# from rest_framework import status
 from networks.models import CustomerIpLeaseModel
 from radiusapp.models import CustomerRadiusSession
 
 
-class FailedSessionStop(APIException):
-    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    default_detail = _('Failed to stop session')
+# class FailedSessionStop(APIException):
+#     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+#     default_detail = _('Failed to stop session')
 
 
 @receiver(pre_delete, sender=CustomerIpLeaseModel)
 def try_stop_session_too(sender, instance, **kwargs):
     sess = CustomerRadiusSession.objects.filter(ip_lease=instance).first()
     if sess:
-        if not sess.finish_session():
-            raise FailedSessionStop
+        sess.finish_session()
+        # if not sess.finish_session():
+            # raise FailedSessionStop
