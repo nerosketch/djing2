2d32ddc040c23d72387c4efed796400f8671d8ee
Dmitry Novikov <nerosketch@gmail.com>
2021-03-17 17:54:30 +0300

    maked tasks for async disconnect customers while managing of their services

---
+++ radiusapp/tasks.py
@@ -0,0 +1,40 @@
+from time import sleep
+from typing import Tuple
+import logging
+from uwsgi_tasks import task
+
+from djing2.lib import safe_int
+from radiusapp.models import CustomerRadiusSession
+
+
+@task()
+def radius_batch_stop_customer_services_task(customer_ids: Tuple[int], delay_interval=100):
+    # TODO: Можно перепроверять сменилась ли услуга у абона,
+    # чтоб понять есть-ли всё ещё смысл его переавторизовывать,
+    # а то к моменту когда до него дойдёт очередь переавторизовываться
+    # он мог уже подключить себе ту же услугу
+    sessions = CustomerRadiusSession.objects.filter(
+        customer_id__in=customer_ids
+    ).only('pk', 'radius_username').iterator()
+    for session in sessions:
+        r = session.finish_session()
+        if r:
+            logging.info('Session "%s" finished' % session)
+        else:
+            logging.info('Session "%s" not finished' % session)
+        sleep(delay_interval / 1000)
+
+
+@task()
+def radius_stop_customer_session_task(customer_id: int, delay_interval=100):
+    customer_id = safe_int(customer_id)
+    session = CustomerRadiusSession.objects.filter(
+        customer_id=customer_id
+    ).first()
+    if session is None:
+        logging.exception('session with customer_id="%d" not found' % customer_id)
+    if session.finish_session():
+        logging.info('Session "%s" finished' % session)
+    else:
+        logging.info('Session "%s" not finished' % session)
+    sleep(delay_interval / 1000)
