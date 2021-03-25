98d687c6a9670e064b60d5245fcdcc54e104daf1
Dmitry Novikov <nerosketch@gmail.com>
2021-03-25 19:05:44 +0300

    added pre commit checks and formatters

--- customers/signals.py
+++ customers/signals.py
@@ -1,15 +1,17 @@
 from datetime import datetime
-from django.dispatch.dispatcher import receiver
+
 from django.db.models.signals import post_init, pre_save
+from django.dispatch.dispatcher import receiver
+
 from customers.models import CustomerService
 
 
 @receiver(post_init, sender=CustomerService)
 def customer_service_post_init(sender, **kwargs):
     customer_service = kwargs["instance"]
-    if getattr(customer_service, 'start_time') is None:
+    if getattr(customer_service, "start_time") is None:
         customer_service.start_time = datetime.now()
-    if getattr(customer_service, 'deadline') is None:
+    if getattr(customer_service, "deadline") is None:
         calc_obj = customer_service.service.get_calc_type()(customer_service)
         customer_service.deadline = calc_obj.calc_deadline()
 
@@ -17,6 +19,6 @@ def customer_service_post_init(sender, **kwargs):
 @receiver(pre_save, sender=CustomerService)
 def customer_service_pre_save(sender, **kwargs):
     customer_service = kwargs["instance"]
-    if getattr(customer_service, 'deadline') is None:
+    if getattr(customer_service, "deadline") is None:
         calc_obj = customer_service.service.get_calc_type()(customer_service)
         customer_service.deadline = calc_obj.calc_deadline()
