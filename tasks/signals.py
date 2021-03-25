d8d6315c595883860174466397cb38743239252d
Dmitry Novikov <nerosketch@gmail.com>
2021-03-26 00:17:53 +0300

    fix

--- tasks/signals.py
+++ tasks/signals.py
@@ -11,23 +11,17 @@ from tasks.models import Task
 def task_post_save(sender, instance: Task, created=False, **kwargs):
     if instance.priority == Task.TASK_PRIORITY_HIGHER:
         if created:
-            notify_text = _('High priority task was created')
+            notify_text = _("High priority task was created")
         else:
-            notify_text = _('High priority task was updated')
-        send_data2ws({
-            'eventType': WsEventTypeEnum.UPDATE_TASK.value,
-            'text': notify_text,
-            'data': {
-                'recipients': [instance.recipients.only(
-                    'pk').values_list('pk', flat=True)],
-                'author': instance.author_id if instance.author else None
+            notify_text = _("High priority task was updated")
+        recipients = instance.recipients.only("pk").values_list("pk", flat=True)
+        send_data2ws(
+            {
+                "eventType": WsEventTypeEnum.UPDATE_TASK.value,
+                "text": notify_text,
+                "data": {"recipients": list(recipients), "author": instance.author_id if instance.author else None},
             }
-        })
-        send_broadcast_push_notification(
-            title=_('Reminders of tasks'),
-            body=notify_text
         )
+        send_broadcast_push_notification(title=_("Reminders of tasks"), body=notify_text)
         return
-    send_data2ws({
-        'eventType': WsEventTypeEnum.UPDATE_TASK.value
-    })
+    send_data2ws({"eventType": WsEventTypeEnum.UPDATE_TASK.value})
