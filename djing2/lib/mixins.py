af5eac26028402a0c4dcbf497f4a4a597f760fcf
Dmitry Novikov <nerosketch@gmail.com>
2021-03-30 11:50:54 +0300

    More informational site error

--- djing2/lib/mixins.py
+++ djing2/lib/mixins.py
@@ -6,9 +6,9 @@ from django.contrib.sites.models import Site
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.decorators import method_decorator
 from django.views.generic.base import View
+from django.http import JsonResponse
 from guardian.shortcuts import get_objects_for_user
 from rest_framework import status
-from rest_framework.exceptions import bad_request
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
@@ -19,19 +19,17 @@ from groupapp.models import Group
 from .decorators import hash_auth_view
 
 
-@method_decorator(hash_auth_view, name='dispatch')
+@method_decorator(hash_auth_view, name="dispatch")
 class HashAuthView(APIView):
-
     def __init__(self, *args, **kwargs):
-        api_auth_secret = getattr(settings, 'API_AUTH_SECRET')
-        if api_auth_secret is None or api_auth_secret == 'your api secret':
-            raise ImproperlyConfigured('You must specified API_AUTH_SECRET in settings')
+        api_auth_secret = getattr(settings, "API_AUTH_SECRET")
+        if api_auth_secret is None or api_auth_secret == "your api secret":
+            raise ImproperlyConfigured("You must specified API_AUTH_SECRET in settings")
         else:
             super().__init__(*args, **kwargs)
 
 
 class AuthenticatedOrHashAuthView(HashAuthView):
-
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             if request.user.is_admin:
@@ -42,14 +40,14 @@ class AuthenticatedOrHashAuthView(HashAuthView):
             return HashAuthView.dispatch(self, request, *args, **kwargs)
 
 
-class AllowedSubnetMixin(object):
+class AllowedSubnetMixin:
     def dispatch(self, request, *args, **kwargs):
         """
         Check if user ip in allowed subnet.
         Return 403 denied otherwise.
         """
-        ip = ip_address(request.META.get('REMOTE_ADDR'))
-        api_auth_subnet = getattr(settings, 'API_AUTH_SUBNET')
+        ip = ip_address(request.META.get("REMOTE_ADDR"))
+        api_auth_subnet = getattr(settings, "API_AUTH_SUBNET")
         if isinstance(api_auth_subnet, str):
             if ip in ip_network(api_auth_subnet):
                 return super().dispatch(request, *args, **kwargs)
@@ -76,16 +74,13 @@ class GroupsFilterMixin:
     Can use only if model has field groups
     groups = models.ManyToManyField(Group)
     """
+
     def get_queryset(self):
         qs = super().get_queryset()
         if self.request.user.is_superuser:
             return qs
         # TODO: May optimize
-        grps = get_objects_for_user(
-            user=self.request.user,
-            perms='groupapp.view_group',
-            klass=Group
-        ).order_by('title')
+        grps = get_objects_for_user(user=self.request.user, perms="groupapp.view_group", klass=Group).order_by("title")
         return qs.filter(groups__in=grps)
 
 
@@ -94,6 +89,7 @@ class SitesFilterMixin:
     Can use only if model has field sites
     sites = models.ManyToManyField(Site)
     """
+
     def get_queryset(self):
         qs = super().get_queryset()
         if self.request.user.is_superuser:
@@ -110,9 +106,8 @@ class SitesGroupFilterMixin(SitesFilterMixin, GroupsFilterMixin):
 
 
 class CustomCurrentSiteMiddleware(CurrentSiteMiddleware):
-
     def process_request(self, request):
         try:
             return super().process_request(request=request)
         except Site.DoesNotExist:
-            return bad_request(request, None)
+            return JsonResponse({"error": "Bad Request (400). Unknown site."}, status=status.HTTP_400_BAD_REQUEST)
