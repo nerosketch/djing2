248db61455dd942d5075082b4b785201c9dca5fc
Dmitry Novikov <nerosketch@gmail.com>
2021-03-30 18:37:15 +0300

    changed signal type

--- networks/signals.py
+++ networks/signals.py
@@ -1,11 +1,11 @@
 from django.dispatch.dispatcher import receiver
-from django.db.models.signals import pre_delete, post_save
+from django.db.models.signals import post_delete, post_save
 
 from djing2.lib.ws_connector import send_data2ws, WsEventTypeEnum
 from networks.models import CustomerIpLeaseModel
 
 
-@receiver(pre_delete, sender=CustomerIpLeaseModel)
+@receiver(post_delete, sender=CustomerIpLeaseModel)
 def on_remove_ip_lease_signal(sender, instance, **kwargs):
     send_data2ws(
         {"eventType": WsEventTypeEnum.UPDATE_CUSTOMER_LEASES.value, "data": {"customer_id": instance.customer_id}}
