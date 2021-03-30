b73ab303e025c71737eb646db599b40334e75878
Dmitry Novikov <nerosketch@gmail.com>
2021-03-30 15:39:48 +0300

    maked server side update signals

--- djing2/lib/ws_connector.py
+++ djing2/lib/ws_connector.py
@@ -5,22 +5,21 @@ from django.conf import settings
 
 
 class WsEventTypeEnum(Enum):
-    UPDATE_TASK = 'updatetask',
-    UPDATEPERMS = 'updateperms',
-    UPDATE_CUSTOMER_LEASES = 'ucls'
+    UPDATE_TASK = "updatetask"
+    UPDATEPERMS = "updateperms"
+    UPDATE_CUSTOMER_LEASES = "ucls"
+    UPDATE_CUSTOMER = "update_customer"
 
 
-def send_data2ws(dat: dict,
-                 host: str = getattr(settings, 'WS_ADDR', '127.0.0.1:3211'),
-                 **kwargs) -> None:
+def send_data2ws(dat: dict, host: str = getattr(settings, "WS_ADDR", "127.0.0.1:3211"), **kwargs) -> None:
     assert isinstance(dat, dict)
-    assert bool(dat.get('eventType'))
+    assert bool(dat.get("eventType"))
     if kwargs:
         dat.update(kwargs)
     dat = dumps(dat)
     try:
         with socket.socket() as s:
-            ipaddr, hport = host.split(':')
+            ipaddr, hport = host.split(":")
             s.connect((ipaddr, int(hport)))
             s.sendall(dat.encode())
     except ConnectionRefusedError:
