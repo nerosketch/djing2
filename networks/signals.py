8a9cc959451ce98afa97a59529ee876045dbf808
Dmitry Novikov <nerosketch@gmail.com>
2021-03-30 17:13:52 +0300

    sig

--- networks/signals.py
+++ networks/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch.dispatcher import receiver
-from django.db.models.signals import pre_delete, post_init
+from django.db.models.signals import pre_delete, post_save
 
 from djing2.lib.ws_connector import send_data2ws, WsEventTypeEnum
 from networks.models import CustomerIpLeaseModel
@@ -7,19 +7,13 @@ from networks.models import CustomerIpLeaseModel
 
 @receiver(pre_delete, sender=CustomerIpLeaseModel)
 def on_remove_ip_lease_signal(sender, instance, **kwargs):
-    send_data2ws({
-        'eventType': WsEventTypeEnum.UPDATE_CUSTOMER_LEASES.value,
-        'data': {
-            'customer_id': instance.customer_id
-        }
-    })
+    send_data2ws(
+        {"eventType": WsEventTypeEnum.UPDATE_CUSTOMER_LEASES.value, "data": {"customer_id": instance.customer_id}}
+    )
 
 
-@receiver(post_init, sender=CustomerIpLeaseModel)
-def on_init_ip_lease_signal(sender, instance, **kwargs):
-    send_data2ws({
-        'eventType': WsEventTypeEnum.UPDATE_CUSTOMER_LEASES.value,
-        'data': {
-            'customer_id': instance.customer_id
-        }
-    })
+@receiver(post_save, sender=CustomerIpLeaseModel)
+def on_post_save_ip_lease_signal(sender, instance, **kwargs):
+    send_data2ws(
+        {"eventType": WsEventTypeEnum.UPDATE_CUSTOMER_LEASES.value, "data": {"customer_id": instance.customer_id}}
+    )
