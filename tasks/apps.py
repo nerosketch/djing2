27c4d8e10f48aeac3ee9950c8808684da06b9e73
Dmitry Novikov <nerosketch@gmail.com>
2021-03-31 12:03:11 +0300

    i found out about the 'noqa'

--- tasks/apps.py
+++ tasks/apps.py
@@ -5,6 +5,4 @@ class TasksConfig(AppConfig):
     name = "tasks"
 
     def ready(self):
-        from tasks import signals
-
-        print("Imported signals", signals)
+        from tasks import signals  # noqa
