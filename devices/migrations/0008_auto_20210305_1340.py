f03c69ea19d8b5fd4be401c9164bc02f5b4b0a42
Dmitry Novikov <nerosketch@gmail.com>
2021-03-31 15:32:40 +0300

    fix

--- devices/migrations/0008_auto_20210305_1340.py
+++ devices/migrations/0008_auto_20210305_1340.py
@@ -6,18 +6,42 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('devices', '0007_device_sites'),
+        ("devices", "0007_device_sites"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='device',
-            name='dev_type',
-            field=models.PositiveSmallIntegerField(choices=[(0, 'Unknown Device'), (1, 'DLink DGS-1100-10/ME'), (2, 'PON OLT'), (3, 'PON ONU BDCOM'), (4, 'Eltex switch'), (5, 'OLT ZTE C320'), (6, 'Zte ONU F660'), (7, 'Zte ONU F601'), (8, 'Huawei switch'), (9, 'DLink DGS-3120-24SC'), (10, 'DLink DGS-1100-06/ME'), (11, 'DLink DGS-3627G'), (12, 'Huawei switch')], default=0, verbose_name='Device type'),
+            model_name="device",
+            name="dev_type",
+            field=models.PositiveSmallIntegerField(
+                choices=[
+                    (0, "Unknown Device"),
+                    (1, "DLink DGS-1100-10/ME"),
+                    (2, "PON OLT"),
+                    (3, "PON ONU BDCOM"),
+                    (4, "Eltex switch"),
+                    (5, "OLT ZTE C320"),
+                    (6, "Zte ONU F660"),
+                    (7, "Zte ONU F601"),
+                    (8, "Huawei switch"),
+                    (9, "DLink DGS-3120-24SC"),
+                    (10, "DLink DGS-1100-06/ME"),
+                    (11, "DLink DGS-3627G"),
+                    (12, "Huawei switch"),
+                ],
+                default=0,
+                verbose_name="Device type",
+            ),
         ),
         migrations.AlterField(
-            model_name='device',
-            name='extra_data',
-            field=models.JSONField(blank=True, help_text='Extra data in JSON format. You may use it for your custom data', null=True, verbose_name='Extra data'),
+            model_name="device",
+            name="extra_data",
+            field=models.JSONField(
+                blank=True,
+                help_text="Extra data in JSON format. You may use it for your custom data",
+                null=True,
+                verbose_name="Extra data",
+            ),
         ),
+        migrations.RunSQL(sql="UPDATE device_dev_type_is_use_dev_port SET is_use_dev_port=true WHERE dev_type=12"),
     ]
