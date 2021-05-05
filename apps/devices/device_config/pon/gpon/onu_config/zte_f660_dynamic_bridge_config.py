1d7938a43d5cff2f35cdf0d8da9c1af666894aaf
Dmitry Novikov <nerosketch@gmail.com>
2021-05-05 14:55:05 +0300

    new onu config

--- apps/devices/device_config/pon/gpon/onu_config/zte_f660_dynamic_bridge_config.py
+++ apps/devices/device_config/pon/gpon/onu_config/zte_f660_dynamic_bridge_config.py
@@ -1,24 +1,25 @@
-from typing import List
-
 from django.utils.translation import gettext_lazy as _
 
 from devices.device_config import expect_util
-from .zte_f601_bridge_config import ZteF601BridgeScriptModule
+from .zte_f660_static_bridge_config import ZteF660BridgeStaticScriptModule, VlanList
 
 
-VlanList = List[int]
-
-
-def _get_onu_template(all_vids: VlanList, *args, **kwargs) -> tuple:
+def _get_onu_template(all_vids: VlanList, onu_mac: str, *args, **kwargs) -> tuple:
     vids = ",".join(map(str, all_vids))
     return (
         "sn-bind enable sn",
         "tcont 1 profile HSI_100",
         "gemport 1 unicast tcont 1 dir both",
-        "security storm-control broadcast rate 8 direction egress vport 1",
-        "security storm-control broadcast rate 8 direction ingress vport 1",
+        "security storm-control broadcast rate 32 direction egress vport 1",
+        "security storm-control broadcast rate 32 direction ingress vport 1",
         "switchport mode trunk vport 1",
         f"switchport vlan {vids} tag vport 1",
+        "port-location format flexible-syntax vport 1",
+        "port-location sub-option remote-id enable vport 1",
+        "port-location sub-option remote-id name %s vport 1" % onu_mac,
+        "dhcp-option82 enable vport 1",
+        "dhcp-option82 trust true replace vport 1",
+        "ip dhcp snooping enable vport 1",
     )
 
 
@@ -84,19 +85,17 @@ def _get_onu_mng_template(all_vids: VlanList, config: dict, *args, **kwargs):
         ]
         + ports_config
         + [
-            "dhcp-ip ethuni eth_0/1 forbidden",
-            "dhcp-ip ethuni eth_0/2 forbidden",
-            "dhcp-ip ethuni eth_0/3 forbidden",
-            "dhcp-ip ethuni eth_0/4 forbidden",
+            "dhcp-ip ethuni eth_0/1 from-internet",
+            "dhcp-ip ethuni eth_0/2 from-internet",
+            "dhcp-ip ethuni eth_0/3 from-internet",
+            "dhcp-ip ethuni eth_0/4 from-internet",
         ]
     )
 
 
-class ZteF660BridgeStaticScriptModule(ZteF601BridgeScriptModule):
-    title = "Zte ONU F660 Static Bridge"
-    short_code = "zte_f660_bridge"
-    accept_vlan = True
-    zte_type = "ZTE-F660"
+class ZteF660BridgeDynamicScriptModule(ZteF660BridgeStaticScriptModule):
+    title = "Zte ONU F660 Dynamic Bridge"
+    short_code = "zte_f660_bridge_d"
 
     def apply_zte_bot_conf(self, get_pon_mng_template_fn=_get_onu_mng_template, *args, **kwargs) -> None:
         return super().apply_zte_bot_conf(get_pon_mng_template_fn=get_pon_mng_template_fn, *args, **kwargs)
