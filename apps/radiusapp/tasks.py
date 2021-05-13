b2911f825473dd370d3f036c6368c4ddb739d12d
Dmitry Novikov <nerosketch@gmail.com>
2021-05-13 15:33:01 +0300

    retry tasks if fail

--- apps/radiusapp/tasks.py
+++ apps/radiusapp/tasks.py
@@ -1,7 +1,7 @@
 """Async tasks for radiusapp."""
 from typing import Tuple
 import logging
-from uwsgi_tasks import task
+from uwsgi_tasks import task, TaskExecutor, SPOOL_OK, SPOOL_RETRY
 
 from radiusapp.models import CustomerRadiusSession
 from .radius_commands import finish_session, change_session_inet2guest, change_session_guest2inet
@@ -15,10 +15,6 @@ def radius_batch_stop_customer_services_task(customer_ids: Tuple[int]):
     :param customer_ids: customers.models.Customer ids
     :return: nothing
     """
-    # TODO: Можно перепроверять сменилась ли услуга у абона,
-    #  чтоб понять есть-ли всё ещё смысл останавливать его услугу,
-    #  а то к моменту, когда до него дойдёт очередь останавливаться,
-    #  он мог уже подключить себе новую услугу.
     sessions = (
         CustomerRadiusSession.objects.filter(customer_id__in=customer_ids).only("pk", "radius_username").iterator()
     )
@@ -29,40 +25,25 @@ def radius_batch_stop_customer_services_task(customer_ids: Tuple[int]):
             logging.info('Session "%s" not changed inet -> guest' % session)
 
 
-# @task()
-# def async_finish_session_task(radius_uname: str):
-#     finish_session(radius_uname)
-
-async_finish_session_task = task(finish_session)
-
-
-# @task()
-# def async_change_session_inet2guest(radius_uname: str):
-#     change_session_inet2guest(
-#         radius_uname=radius_uname
-#     )
-
-#
-# Call like this:
-# async_change_session_inet2guest(radius_uname: str)
-#
-async_change_session_inet2guest = task(change_session_inet2guest)
+@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_count=3, retry_timeout=5)
+def async_finish_session_task(radius_uname: str):
+    try:
+        if finish_session(radius_uname):
+            return SPOOL_OK
+    except Exception:
+        pass
+    return SPOOL_RETRY
 
 
-# @task()
-# def async_change_session_guest2inet(
-#     radius_uname: str,
-#     speed_in: int,
-#     speed_out: int,
-#     speed_in_burst: int,
-#     speed_out_burst: int):
-#     change_session_guest2inet(
-#         radius_uname=radius_uname,
-#         speed_in=speed_in,
-#         speed_out=speed_out,
-#         speed_in_burst=speed_in_burst,
-#         speed_out_burst=speed_out_burst
-#     )
+@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_count=2, retry_timeout=5)
+def async_change_session_inet2guest(radius_uname: str):
+    try:
+        if change_session_inet2guest(radius_uname):
+            return SPOOL_OK
+    except Exception:
+        pass
+    return SPOOL_RETRY
+
 
 #
 # Call like this:
@@ -74,4 +55,11 @@ async_change_session_inet2guest = task(change_session_inet2guest)
 #     speed_out_burst: int
 # )
 #
-async_change_session_guest2inet = task(change_session_guest2inet)
+@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_count=2, retry_timeout=5)
+def async_change_session_guest2inet(*args, **kwargs):
+    try:
+        if change_session_guest2inet(*args, **kwargs):
+            return SPOOL_OK
+    except Exception:
+        pass
+    return SPOOL_RETRY
