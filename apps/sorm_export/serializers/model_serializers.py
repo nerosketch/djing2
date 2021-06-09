cee108d13d284abe3abc6a8efcb71826f8f55b48
Dmitry Novikov <nerosketch@gmail.com>
2021-06-09 18:04:14 +0300

    validating for unique relation group to FIAS addresses

--- apps/sorm_export/serializers/model_serializers.py
+++ apps/sorm_export/serializers/model_serializers.py
@@ -1,5 +1,9 @@
+from django.db.models import Count
+from django.utils.translation import gettext as _
 from rest_framework import serializers
-from sorm_export import models
+from rest_framework.exceptions import ValidationError
+from groupapp.models import Group
+from sorm_export.models import FiasRecursiveAddressModel
 
 
 class FiasRecursiveAddressModelSerializer(serializers.ModelSerializer):
@@ -7,6 +11,22 @@ class FiasRecursiveAddressModelSerializer(serializers.ModelSerializer):
     ao_type_name = serializers.CharField(source='get_ao_type_display', read_only=True)
     parent_ao_name = serializers.CharField(source='parent_ao.title', read_only=True)
 
+    def validate(self, attrs):
+        """Group must have only one FiasRecursiveAddressModel.
+        And specifying ForeignKey in Group model may be corrupt
+        flexibility. So I did this validation here."""
+        selected_group_ids = [sg.pk for sg in attrs.get('groups', [])]
+        groups = Group.objects.filter(
+            pk__in=selected_group_ids
+        ).annotate(
+            addrs_count=Count('fiasrecursiveaddressmodel')
+        ).filter(addrs_count__gt=0)
+        if groups.exists():
+            raise ValidationError({
+                'groups': _('Groups "%s" has more than one "FIAS" address type.') % ','.join(g.title for g in groups)
+            })
+        return attrs
+
     class Meta:
-        model = models.FiasRecursiveAddressModel
+        model = FiasRecursiveAddressModel
         fields = '__all__'
