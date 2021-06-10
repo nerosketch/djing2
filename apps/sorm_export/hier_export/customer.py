f94a9a99be657d4536341e6f76eda3260e9601ea
Dmitry Novikov <nerosketch@gmail.com>
2021-06-10 16:45:49 +0300

    almost ready exports

--- apps/sorm_export/hier_export/customer.py
+++ apps/sorm_export/hier_export/customer.py
@@ -131,7 +131,7 @@ def export_access_point_address(customers: Iterable[Customer], event_time=None):
 
 
 @iterable_export_decorator
-def export_individual_customer(customers: Iterable[Customer], event_time=None):
+def export_individual_customer(customers_queryset, event_time=None):
     """
     Файл выгрузки данных о физическом лице, версия 1
     В этом файле выгружается информация об абонентах, у которых контракт заключён с физическим лицом.
@@ -144,15 +144,14 @@ def export_individual_customer(customers: Iterable[Customer], event_time=None):
         if not hasattr(customer, 'group'):
             return
         group = customer.group
-        if not hasattr(group, 'fiasrecursiveaddressmodel') or not hasattr(group.fiasrecursiveaddressmodel, 'fias_recursive_address'):
-            return
-        addr_group = group.fiasrecursiveaddressmodel.fias_recursive_address
-        if not addr_group.parent_ao:
+
+        addr_group = group.fiasrecursiveaddressmodel_set.first()
+        if addr_group is None:
             return
         passport = customer.passportinfo
         create_date = customer.create_date
         return {
-            'customer_id': customer.pk,
+            'contract_id': customer.pk,
             'name': customer.fio,
             'surname': customer.get_full_name(),
             'birthday': customer.birth_day,
@@ -160,24 +159,21 @@ def export_individual_customer(customers: Iterable[Customer], event_time=None):
             'document_serial': passport.series,
             'document_number': passport.number,
             'document_distributor': passport.distributor,
-            # TODO: Код подразделения негде брать
-            # 'passport_code': passport.,
+            'passport_code': passport.division_code or '',
             'passport_date': passport.date_of_acceptance,
             'house': customer.house,
-            'parent_id_ao': addr_group.parent_ao_id,
+            'parent_id_ao': addr_group.pk,
             'actual_start_time': datetime(create_date.year, create_date.month, create_date.day),
             # 'actual_end_time':
+            'customer_id': customer.pk
         }
 
     return (
         individual_entity_serializers.CustomerIndividualObjectFormat,
-        gen, customers
-            .exclude(passportinfo=None)
-            .select_related(
-            'group', 'group__fiasrecursiveaddressmodel',
-            'group__fiasrecursiveaddressmodel__fias_recursive_address'
+        gen, customers_queryset.exclude(passportinfo=None).select_related(
+            'group', 'passportinfo'
         ),
-        f'ISP/abonents/fiz_v1_{format_fname(event_time)}.txt'
+        f'ISP/abonents/fiz_v2_{format_fname(event_time)}.txt'
     )
 
 
