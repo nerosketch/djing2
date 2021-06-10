116b7c1e248d6030ed8058444a266b7f5ce98463
Dmitry Novikov <nerosketch@gmail.com>
2021-06-10 17:24:33 +0300

    maked payment log export

--- apps/sorm_export/signals/payment.py
+++ apps/sorm_export/signals/payment.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 
 from fin_app.models import AllTimePayLog
 from sorm_export.tasks.payment import export_customer_payment_task
 
 
-@receiver(post_delete, sender=AllTimePayLog)
-def customer_payment_signal(sender, instance=None, *args, **kwargs):
-    print('signal customer_payment_signal', args, kwargs)
+@receiver(post_save, sender=AllTimePayLog)
+def alltime_payment_signal(sender, instance, created=False, *args, **kwargs):
+    # print('signal customer_payment_signal', created, args, kwargs)
 
-    export_customer_payment_task(
-        customer_lease_id_list=[instance.pk],
-        event_time=str(datetime.now())
-    )
+    if created:
+        export_customer_payment_task(
+            pay_log_id_list=[instance.pk],
+            event_time=str(datetime.now())
+        )
