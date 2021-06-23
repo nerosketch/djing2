8f06dde244a818c47149e99ddb046aac99d2cc8a
Dmitry Novikov <nerosketch@gmail.com>
2021-06-23 14:51:46 +0300

    fixes

--- apps/sorm_export/signals/networks.py
+++ apps/sorm_export/signals/networks.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 from networks.models import CustomerIpLeaseModel
 from sorm_export.tasks.networks import export_ip_leases_task
@@ -18,7 +18,7 @@ def customer_ip_changed(sender, instance: Optional[CustomerIpLeaseModel] = None,
         )
 
 
-@receiver(post_delete, sender=CustomerIpLeaseModel)
-def customer_ip_deleted(sender, *args, **kwargs):
-    print('signal customer_ip_deleted', args, kwargs)
-    # TODO: узнать можно-ли удалять ip
+# @receiver(post_delete, sender=CustomerIpLeaseModel)
+# def customer_ip_deleted(sender, *args, **kwargs):
+#    print('signal customer_ip_deleted', args, kwargs)
+# TODO: узнать можно-ли удалять ip
