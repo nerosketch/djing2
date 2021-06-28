e56a388400766356c32b5641cae410c4a48d9cef
Dmitry Novikov <nerosketch@gmail.com>
2021-06-28 16:31:58 +0300

    maked comments app

---
+++ apps/customer_comments/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from customers.models import Customer
+from profiles.models import UserProfile
+
+
+class CustomerCommentModel(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, verbose_name=_("Author"), on_delete=models.CASCADE)
+    text = models.TextField(_('Text'))
+    create_time = models.DateTimeField(_('Comment time'), auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        db_table = 'customer_comments'
