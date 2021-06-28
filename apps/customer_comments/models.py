e330897b97faadfde2eb5aad15c4beafffbc68e2
Dmitry Novikov <nerosketch@gmail.com>
2021-06-28 21:13:38 +0300

    renamed creation time field

--- apps/customer_comments/models.py
+++ apps/customer_comments/models.py
@@ -7,11 +7,11 @@ from profiles.models import UserProfile
 class CustomerCommentModel(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     author = models.ForeignKey(UserProfile, verbose_name=_("Author"), on_delete=models.CASCADE)
-    text = models.TextField(_('Text'))
-    create_time = models.DateTimeField(_('Comment time'), auto_now_add=True)
+    text = models.TextField(_("Text"))
+    date_create = models.DateTimeField(_("Comment time"), auto_now_add=True)
 
     def __str__(self):
         return self.text
 
     class Meta:
-        db_table = 'customer_comments'
+        db_table = "customer_comments"
