753f16e0f62681882daac9f96a7b9dee56b7648e
Dmitry Novikov <nerosketch@gmail.com>
2021-07-09 10:28:15 +0300

    maked aaa export

--- apps/sorm_export/tasks/aaa.py
+++ apps/sorm_export/tasks/aaa.py
@@ -1,7 +1,9 @@
 import csv
 from datetime import datetime
-from uwsgi_tasks import task
+from uwsgi_tasks import task, cron
 from sorm_export.serializers.aaa import AAAExportSerializer, AAA_EXPORT_FNAME
+from sorm_export.ftp_worker.func import send_file2ftp
+from sorm_export.base import format_fname
 
 
 @task()
@@ -10,3 +12,9 @@ def save_radius_acct_start(event_time: datetime, data: AAAExportSerializer) -> N
     with open(AAA_EXPORT_FNAME, "a") as f:
         csv_writer = csv.writer(f, dialect="unix", delimiter=";")
         csv_writer.writerow(line)
+
+
+@cron(minute=-15)
+def upload_aaa_2_ftp(signal_number):
+    now = datetime.now()
+    send_file2ftp(fname=AAA_EXPORT_FNAME, remote_fname=f"ISP/aaa/aaa_v1_{format_fname(now)}.txt")
