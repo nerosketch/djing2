a9281481d6dd0dad11d4bd5198207cfc08073ddc
Dmitry Novikov <nerosketch@gmail.com>
2021-07-09 14:16:57 +0300

    fix

--- apps/sorm_export/ftp_worker/func.py
+++ apps/sorm_export/ftp_worker/func.py
@@ -41,5 +41,5 @@ def send_text_buf2ftp(fp: TextIOWrapper, remote_fname: str) -> None:
 
 @_ftp_credentials
 def send_file2ftp(ftp, fname: str, remote_fname: str) -> None:
-    with open(fname) as file:
+    with open(fname, 'r') as file:
         ftp.storebinary("STOR %s" % remote_fname, file)
