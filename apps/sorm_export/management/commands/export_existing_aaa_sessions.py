9f2f66526433a257532ae93f404cc08e1d295c13
Dmitry Novikov <nerosketch@gmail.com>
2021-07-12 10:46:12 +0300

    maked all aaa export

--- apps/sorm_export/management/commands/export_existing_aaa_sessions.py
+++ apps/sorm_export/management/commands/export_existing_aaa_sessions.py
@@ -4,7 +4,6 @@ from netfields.mac import mac_unix_common
 
 from django.core.management.base import BaseCommand
 from radiusapp.models import CustomerRadiusSession
-from rest_framework.utils.serializer_helpers import ReturnList
 from sorm_export.serializers.aaa import AAAExportSerializer, AAAEventType
 
 
@@ -17,8 +16,9 @@ class Command(BaseCommand):
         )
 
     def handle(self, fname=None, *args, **options):
-        sessions = CustomerRadiusSession.objects.exclude(customer=None).defer(
-            'last_event_time', 'session_duration', 'input_packets', 'output_packets', 'closed'
+        sessions = CustomerRadiusSession.objects.exclude(customer=None).only(
+            'customer', 'assign_time', 'session_id', 'ip_lease',
+            'input_octets', 'output_octets'
         ).select_related('customer', 'ip_lease').iterator()
 
         dat = [{
@@ -27,23 +27,23 @@ class Command(BaseCommand):
             "session_id": str(ses.session_id),
             "customer_ip": ses.ip_lease.ip_address,
             "customer_db_username": ses.customer.get_username(),
+            'input_octets': ses.input_octets,
+            'output_octets': ses.output_octets,
             "customer_device_mac": ses.ip_lease.mac_address.format(dialect=mac_unix_common) if ses.ip_lease.mac_address else '00:00:00:00:00'
         } for ses in sessions]
 
         ser = AAAExportSerializer(data=dat, many=True)
         ser.is_valid(raise_exception=True)
 
-        print(ser.data[0], type(ser.data))
-        lines = [v for v in ser.data]
-        print('Lines:', lines)
+        lines_gen = ((v for k,v in i.items()) for i in ser.data)
 
         if fname is None:
             csv_writer = csv.writer(sys.stdout, dialect="unix", delimiter=";")
-            csv_writer.writerows(lines)
+            csv_writer.writerows(lines_gen)
         else:
             with open(fname, "a") as f:
                 csv_writer = csv.writer(f, dialect="unix", delimiter=";")
-                csv_writer.writerows(lines)
+                csv_writer.writerows(lines_gen)
 
-        self.stdout.write('Result writes to "%s" ', ending='')
-        self.stdout.write(self.style.SUCCESS('OK'))
+            self.stdout.write('Result writes ', ending='')
+            self.stdout.write(self.style.SUCCESS('OK'))
