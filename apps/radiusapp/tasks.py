8cd5c43eb32e1f6d430b57210ee63930507310bf
Dmitry Novikov <nerosketch@gmail.com>
2021-07-14 18:55:27 +0300

    mk

--- apps/radiusapp/tasks.py
+++ apps/radiusapp/tasks.py
@@ -1,13 +1,13 @@
 """Async tasks for radiusapp."""
 from typing import Tuple
 import logging
-from uwsgi_tasks import task, TaskExecutor, SPOOL_OK
+from uwsgi_tasks import task, TaskExecutor, SPOOL_OK, SPOOL_RETRY
 
 from radiusapp.models import CustomerRadiusSession
-from .radius_commands import finish_session, change_session_inet2guest, change_session_guest2inet
+from radiusapp import radius_commands as rc
 
 
-@task()
+@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=30)
 def radius_batch_stop_customer_services_task(customer_ids: Tuple[int]):
     """
     Async COA RADIUS sessions inet->guest.
@@ -15,25 +15,41 @@ def radius_batch_stop_customer_services_task(customer_ids: Tuple[int]):
     :param customer_ids: customers.models.Customer ids
     :return: nothing
     """
+    # FIXME: pass to task only data, not params for queryset
     sessions = (
         CustomerRadiusSession.objects.filter(customer_id__in=customer_ids).only("pk", "radius_username").iterator()
     )
     for session in sessions:
-        if session.radius_coa_inet2guest():
-            logging.info('Session "%s" changed inet -> guest' % session)
-        else:
-            logging.info('Session "%s" not changed inet -> guest' % session)
-
-
-@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_count=3, retry_timeout=5)
-def async_finish_session_task(radius_uname: str):
-    finish_session(radius_uname)
+        try:
+            if not session.radius_coa_inet2guest():
+                logging.info('Session "%s" not changed inet -> guest' % session)
+        except rc.RadiusBaseException as err:
+            logging.error(str(err))
     return SPOOL_OK
 
 
-@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_count=2, retry_timeout=5)
+Доделать обработку исключений
+
+@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=5)
+def async_finish_session_task(radius_uname: str):
+    try:
+        ret_text = rc.finish_session(radius_uname)
+        if ret_text is not None:
+            logging.warning(ret_text)
+        return SPOOL_OK
+    except rc.RadiusSessionNotFoundException as e:
+        return SPOOL_OK
+    except rc.RadiusTimeoutException as e:
+        return SPOOL_RETRY
+    except rc.RadiusInvalidRequestException as e:
+        pass
+    except rc.RadiusMissingAttributeException as e:
+        pass
+
+
+@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=5)
 def async_change_session_inet2guest(radius_uname: str):
-    change_session_inet2guest(radius_uname)
+    rc.change_session_inet2guest(radius_uname)
     return SPOOL_OK
 
 
@@ -47,7 +63,7 @@ def async_change_session_inet2guest(radius_uname: str):
 #     speed_out_burst: int
 # )
 #
-@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_count=2, retry_timeout=5)
+@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=5)
 def async_change_session_guest2inet(*args, **kwargs):
-    change_session_guest2inet(*args, **kwargs)
+    rc.change_session_guest2inet(*args, **kwargs)
     return SPOOL_OK
