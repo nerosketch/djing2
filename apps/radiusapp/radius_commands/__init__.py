d264dc89ee2874c73ef3b264ada2e1a576be2065
Dmitry Novikov <nerosketch@gmail.com>
2021-07-14 14:02:49 +0300

    maked module 4 radius

---
+++ apps/radiusapp/radius_commands/__init__.py
@@ -0,0 +1,7 @@
+from .radius_commands import (
+    change_session_guest2inet,
+    change_session_inet2guest,
+    finish_session
+)
+
+__all__ = ['change_session_guest2inet', 'change_session_inet2guest', 'finish_session']
