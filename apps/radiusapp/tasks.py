098b01ea64dcf92fe6363c2c9d21906be8e44e5c
Dmitry Novikov <nerosketch@gmail.com>
2021-07-15 13:27:52 +0300

    done

--- apps/radiusapp/tasks.py
+++ apps/radiusapp/tasks.py
@@ -1,10 +1,8 @@
 """Async tasks for radiusapp."""
 from functools import wraps
-from typing import Tuple
 import logging
 from uwsgi_tasks import task, TaskExecutor, SPOOL_OK, SPOOL_RETRY
 
-from radiusapp.models import CustomerRadiusSession
 from radiusapp import radius_commands as rc
 
 
@@ -25,28 +23,6 @@ def _radius_task_error_wrapper(fn):
     return _wrapped
 
 
-@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=30)
-@_radius_task_error_wrapper
-def radius_batch_stop_customer_services_task(customer_ids: Tuple[int]):
-    """
-    Async COA RADIUS sessions inet->guest.
-
-    :param customer_ids: customers.models.Customer ids
-    :return: nothing
-    """
-    # FIXME: pass to task only data, not params for queryset
-    sessions = (
-        CustomerRadiusSession.objects.filter(customer_id__in=customer_ids).only("pk", "radius_username").iterator()
-    )
-    for session in sessions:
-        try:
-            if not session.radius_coa_inet2guest():
-                logging.info('Session "%s" not changed inet -> guest' % session)
-        except rc.RadiusBaseException as err:
-            logging.error(str(err))
-    return SPOOL_OK
-
-
 @task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=5)
 @_radius_task_error_wrapper
 def async_finish_session_task(radius_uname: str):
@@ -56,9 +32,15 @@ def async_finish_session_task(radius_uname: str):
     return SPOOL_OK
 
 
-@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=5)
+@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=15)
 @_radius_task_error_wrapper
 def async_change_session_inet2guest(radius_uname: str):
+    """
+    Async COA RADIUS sessions inet->guest.
+
+    :param radius_uname: radius User-Name value
+    :return: nothing
+    """
     ret_text = rc.change_session_inet2guest(radius_uname)
     if ret_text is not None:
         logging.warning(ret_text)
@@ -75,7 +57,7 @@ def async_change_session_inet2guest(radius_uname: str):
 #     speed_out_burst: int
 # )
 #
-@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=5)
+@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=15)
 @_radius_task_error_wrapper
 def async_change_session_guest2inet(*args, **kwargs):
     ret_text = rc.change_session_guest2inet(*args, **kwargs)
