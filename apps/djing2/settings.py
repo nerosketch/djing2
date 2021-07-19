8a79b9b3dc7f3c7cf5175ac7a4df3c6b46e9bb89
Dmitry Novikov <nerosketch@gmail.com>
2021-07-19 15:13:55 +0300

    maked show_webhook action

--- apps/djing2/settings.py
+++ apps/djing2/settings.py
@@ -233,6 +233,9 @@ API_AUTH_SUBNET = getattr(local_settings, "API_AUTH_SUBNET", ("127.0.0.0/8", "10
 # Company name
 COMPANY_NAME = getattr(local_settings, "COMPANY_NAME", "Company Name")
 
+# public url for messenger bot
+MESSENGER_BOT_PUBLIC_URL = getattr(local_settings, 'MESSENGER_BOT_PUBLIC_URL', "https://localhost.name")
+
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "djing2.lib.paginator.QueryPageNumberPagination",
     # 'PAGE_SIZE': 180,
