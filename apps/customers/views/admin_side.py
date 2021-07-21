f78c2570bc1328a3507504415114c608c084b36e
Dmitry Novikov <nerosketch@gmail.com>
2021-07-21 18:48:02 +0300

    maked customers activity report access by rights

--- apps/customers/views/admin_side.py
+++ apps/customers/views/admin_side.py
@@ -330,6 +330,7 @@ class CustomerModelViewSet(SitesFilterMixin, DjingModelViewSet):
 
     @action(detail=False)
     def activity_report(self, request):
+        self.check_permission_code(request, "customers.can_view_activity_report")
         r = models.Customer.objects.activity_report()
         return Response(r)
 
