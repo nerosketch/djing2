6b21f671604ec0de7309a6c501e63a8d352ba429
Dmitry Novikov <nerosketch@gmail.com>
2021-07-28 14:24:47 +0300

    allow empty devices for dot

--- apps/maps/serializers.py
+++ apps/maps/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 
+from devices.models import Device
 from djing2.lib.mixins import BaseCustomModelSerializer
 from maps.models import DotModel
 
 
 class DotModelSerializer(BaseCustomModelSerializer):
+    devices = serializers.PrimaryKeyRelatedField(
+        many=True,
+        allow_empty=True,
+        queryset=Device.objects.all()
+    )
 
     class Meta:
         model = DotModel
