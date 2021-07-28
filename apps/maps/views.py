dab3c569b0b501ec24ccb5b5952ce2222e3680f7
Dmitry Novikov <nerosketch@gmail.com>
2021-07-28 22:58:35 +0300

    filter dot devices

--- apps/maps/views.py
+++ apps/maps/views.py
@@ -1,3 +1,7 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from devices.serializers import DeviceModelSerializer
 from djing2.viewsets import DjingModelViewSet
 from maps.models import DotModel
 from maps.serializers import DotModelSerializer
@@ -6,3 +10,12 @@ from maps.serializers import DotModelSerializer
 class DotModelViewSet(DjingModelViewSet):
     queryset = DotModel.objects.all()
     serializer_class = DotModelSerializer
+
+    @action(methods=['get'], detail=True)
+    def get_devs(self, request, pk=None):
+        obj = self.get_object()
+        devs = obj.devices.all()
+        devs_ser = DeviceModelSerializer(instance=devs, many=True, context={
+            'request': request
+        })
+        return Response(devs_ser.data)
