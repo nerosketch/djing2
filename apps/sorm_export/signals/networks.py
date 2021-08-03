ecd259ad6d816cb8f976211ded659fd9621db936
Dmitry Novikov <nerosketch@gmail.com>
2021-08-03 15:42:25 +0300

    export ip lease delete

--- apps/sorm_export/signals/networks.py
+++ apps/sorm_export/signals/networks.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 from typing import Optional
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch.dispatcher import receiver
 from networks.models import CustomerIpLeaseModel
-from sorm_export.tasks.networks import export_ip_leases_task
+from sorm_export.tasks.networks import (
+    export_static_ip_leases_task,
+    export_static_ip_leases_task_finish
+)
 
 
 @receiver(post_save, sender=CustomerIpLeaseModel)
@@ -12,13 +15,19 @@ def customer_ip_changed(sender, instance: Optional[CustomerIpLeaseModel] = None,
     if not instance.is_dynamic:
         # only if lease is static
         # https://wiki.vasexperts.ru/doku.php?id=sorm:sorm3:sorm3_subs_dump:sorm3_subs_ip_nets:start
-        export_ip_leases_task(
+        export_static_ip_leases_task(
             customer_lease_id_list=[instance.pk],
             event_time=str(datetime.now())
         )
 
 
-# @receiver(post_delete, sender=CustomerIpLeaseModel)
-# def customer_ip_deleted(sender, *args, **kwargs):
-#    print('signal customer_ip_deleted', args, kwargs)
-# TODO: узнать можно-ли удалять ip
+@receiver(pre_delete, sender=CustomerIpLeaseModel)
+def customer_ip_deleted(sender, instance, *args, **kwargs):
+    if not instance.is_dynamic:
+        export_static_ip_leases_task_finish(
+            customer_id=instance.customer_id,
+            ip_address=instance.ip_address,
+            lease_time=instance.lease_time,
+            mac_address=instance.mac_address,
+            event_time=datetime.now()
+        )
