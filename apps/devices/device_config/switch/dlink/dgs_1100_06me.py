dec0dea02c438f9cc46919887e57fbb32e81722f
Dmitry Novikov <nerosketch@gmail.com>
2021-09-02 19:43:28 +0300

    maked all dlinks

--- apps/devices/device_config/switch/dlink/dgs_1100_06me.py
+++ apps/devices/device_config/switch/dlink/dgs_1100_06me.py
@@ -1,6 +1,10 @@
 from devices.device_config.base import Vlans, Vlan
 from djing2.lib import safe_int
 from .dgs_3120_24sc import DlinkDGS_3120_24SCSwitchInterface
+from .. import SwitchDeviceStrategyContext
+from ...base_device_strategy import SNMPWorker
+
+_DEVICE_UNIQUE_CODE = 10
 
 
 class DlinkDGS_1100_06MESwitchInterface(DlinkDGS_3120_24SCSwitchInterface):
@@ -10,7 +14,9 @@ class DlinkDGS_1100_06MESwitchInterface(DlinkDGS_3120_24SCSwitchInterface):
     ports_len = 6
 
     def read_all_vlan_info(self) -> Vlans:
-        vids = self.get_list_keyval(".1.3.6.1.4.1.171.10.134.1.1.7.6.1.1")
+        dev = self.model_instance
+        with SNMPWorker(hostname=dev.ip_address, community=str(dev.man_passw)) as snmp:
+            vids = snmp.get_list_keyval(".1.3.6.1.4.1.171.10.134.1.1.7.6.1.1")
         for vid_name, vid in vids:
             vid = safe_int(vid)
             if vid in (0, 1):
@@ -20,22 +26,24 @@ class DlinkDGS_1100_06MESwitchInterface(DlinkDGS_3120_24SCSwitchInterface):
     def read_port_vlan_info(self, port: int) -> Vlans:
         if port > self.ports_len or port < 1:
             raise ValueError("Port must be in range 1-%d" % self.ports_len)
-        vids = self.get_list_keyval(".1.3.6.1.4.1.171.10.134.1.1.7.6.1.1")
-        native_vid = self.get_item(".1.3.6.1.4.1.171.10.134.1.1.7.7.1.1.%d" % port)
-        if not native_vid:
-            return
-        for vidname, vid in vids:
-            vid = safe_int(vid)
-            if vid in (0, 1):
-                continue
-            member_ports = self.get_item(".1.3.6.1.4.1.171.10.134.1.1.7.6.1.2.%d" % vid)
-            if member_ports is None:
+        dev = self.model_instance
+        with SNMPWorker(hostname=dev.ip_address, community=str(dev.man_passw)) as snmp:
+            vids = snmp.get_list_keyval(".1.3.6.1.4.1.171.10.134.1.1.7.6.1.1")
+            native_vid = snmp.get_item(".1.3.6.1.4.1.171.10.134.1.1.7.7.1.1.%d" % port)
+            if not native_vid:
                 return
-            member_ports = self._make_ports_map(member_ports[:4])
-            if not member_ports[port - 1]:
-                # if port num is not <port>
-                continue
-            yield Vlan(vid=vid, title=vidname, native=(vid == native_vid))
+            for vidname, vid in vids:
+                vid = safe_int(vid)
+                if vid in (0, 1):
+                    continue
+                member_ports = snmp.get_item(".1.3.6.1.4.1.171.10.134.1.1.7.6.1.2.%d" % vid)
+                if member_ports is None:
+                    return
+                member_ports = self._make_ports_map(member_ports[:4])
+                if not member_ports[port - 1]:
+                    # if port num is not <port>
+                    continue
+                yield Vlan(vid=vid, title=vidname, native=(vid == native_vid))
 
     # def attach_vlan_to_port(self, vid: int, port: int, tag: bool = True) -> bool:
     #     if port > self.ports_len or port < 1:
@@ -59,4 +67,5 @@ class DlinkDGS_1100_06MESwitchInterface(DlinkDGS_3120_24SCSwitchInterface):
     #     out = self.read_until(self.prompt)
     #     return b'Success' in out
 
-SwitchDeviceStrategyContext.add_device_type(_DEVICE_UNIQUE_CODE, DlinkDGS_3120_24SCSwitchInterface)
+
+SwitchDeviceStrategyContext.add_device_type(_DEVICE_UNIQUE_CODE, DlinkDGS_1100_06MESwitchInterface)
