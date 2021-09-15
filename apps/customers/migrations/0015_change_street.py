ebe46f7a3f15e2bb8f2f075a8a4898a9fc2845f2
Dmitry Novikov <nerosketch@gmail.com>
2021-09-15 17:14:13 +0300

    fixed data transfer

--- apps/customers/migrations/0015_change_street.py
+++ apps/customers/migrations/0015_change_street.py
@@ -11,23 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql="""begin;
-create temp table old_streets on commit drop as
-select
-       ls.id as locality_street_id,
-       g.id as group_id,
-       l.id as locality_id,
-       c.baseaccount_ptr_id as customers_id
-from customer_street cs
-         left join groups g on cs.group_id = g.id
-         left join locality l on l.title = g.title
-         left join locality_street ls on l.id = ls.locality_id
-         left join customers c on cs.id = c.street_id
-where ls.name = cs.name;
-update customers set street_id = null;
-"""
-        ),
         migrations.AlterField(
             model_name='customer',
             name='street',
@@ -39,21 +22,10 @@ update customers set street_id = null;
             field=models.ForeignKey(blank=True, default=None, null=True, on_delete=models.deletion.SET_NULL,
                                     to='addresses.localitymodel'),
         ),
-        migrations.RunSQL(
-            sql="""
-            update customers
-set street_id = o.locality_street_id,
-locality_id = o.locality_id
-from (
-         select os.locality_street_id, os.group_id, os.locality_id, os.customers_id
-         from old_streets os
-     ) as o
-where customers.group_id = o.group_id
-           and o.customers_id = customers.baseaccount_ptr_id;
-
-commit;"""
-        ),
         migrations.DeleteModel(
             name='CustomerStreet',
         ),
+        migrations.RunSQL(
+            sql="update customers set locality_id = group_id;"
+        )
     ]
