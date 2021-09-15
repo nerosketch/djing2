482a7c18776a439623dbe87f26a782ce1de913ca
Dmitry Novikov <nerosketch@gmail.com>
2021-09-15 11:16:43 +0300

    fix

--- apps/sorm_export/migrations/0003_auto_20210914_1803.sql
+++ apps/sorm_export/migrations/0003_auto_20210914_1803.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW get_streets_as_addr_objects AS
   SELECT cs.id AS street_id, sa.id AS parent_ao_id, sa.ao_type AS parent_ao_type, cs.name AS street_name
   FROM locality_street cs
-    INNER JOIN sorm_address_groups sag ON (sag.group_id = cs.locality_id)
-    INNER JOIN sorm_address sa ON (sa.id = sag.fiasrecursiveaddressmodel_id)
+    INNER JOIN sorm_address_localities sal ON (sal.localitymodel_id = cs.locality_id)
+    INNER JOIN sorm_address sa ON (sa.id = sal.fiasrecursiveaddressmodel_id)
   ORDER BY sa.ao_level;
