88d6bee0638ca652aa08c2206b5b5b05ef6d2e73
Dmitry Novikov <nerosketch@gmail.com>
2021-09-17 14:41:00 +0300

    fix sites show

--- apps/addresses/views.py
+++ apps/addresses/views.py
@@ -1,9 +1,10 @@
 from djing2.viewsets import DjingModelViewSet
 from addresses.models import LocalityModel, StreetModel
 from addresses.serializers import LocalityModelSerializer, StreetModelSerializer
+from djing2.lib.mixins import SitesFilterMixin
 
 
-class LocalityModelViewSet(DjingModelViewSet):
+class LocalityModelViewSet(SitesFilterMixin, DjingModelViewSet):
     queryset = LocalityModel.objects.order_by('title')
     serializer_class = LocalityModelSerializer
 
