881132b5a59a49fd106ef97383fb8b936b28c718
Dmitry Novikov <nerosketch@gmail.com>
2021-09-20 00:28:06 +0300

    fix

--- apps/messenger/urls.py
+++ apps/messenger/urls.py
@@ -7,12 +7,13 @@ from messenger.models.base_messenger import get_messenger_model_info_generator
 router = DefaultRouter()
 router.register('subscriber', views.SubscriberModelViewSet)
 
+bvs = views.MessengerModelViewSet
 for type_name, messenger_uint, messenger_model_class in get_messenger_model_info_generator():
-    tmp_viewset = views.MessengerModelViewSet
-    tmp_viewset.queryset = messenger_model_class.objects.all()
+    tmp_viewset = type('tmp_viewset', bvs.__bases__, dict(bvs.__dict__))
+    tmp_viewset.queryset = messenger_model_class.objects.filter(bot_type=messenger_uint)
     tmp_viewset.serializer_class.Meta.model = messenger_model_class
     router.register(type_name, tmp_viewset, basename=f'messenger-{type_name}')
-
+del bvs, tmp_viewset, type_name, messenger_uint, messenger_model_class
 
 app_name = "messenger"
 
