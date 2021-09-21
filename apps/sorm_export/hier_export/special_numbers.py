372e8a0b09a33b5465e3d4fa95b1e397d23ce4bb
Dmitry Novikov <nerosketch@gmail.com>
2021-09-21 13:23:54 +0300

    fix numbers export

--- apps/sorm_export/hier_export/special_numbers.py
+++ apps/sorm_export/hier_export/special_numbers.py
@@ -5,7 +5,6 @@ store_fname = './apps/sorm_export/special_numbers.csv'
 
 
 def export_special_numbers(event_time=None):
-
     send_file2ftp(
         fname=store_fname,
         remote_fname=f"ISP/dict/special_numbers_{format_fname(event_time)}.txt"
