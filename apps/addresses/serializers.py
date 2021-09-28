98d5f61a93aeab4f5368d878739b07dbe2d50111
Dmitry Novikov <nerosketch@gmail.com>
2021-09-28 11:31:48 +0300

    mk parent

--- apps/addresses/serializers.py
+++ apps/addresses/serializers.py
@@ -1,8 +1,11 @@
+from rest_framework import serializers
 from djing2.lib.mixins import BaseCustomModelSerializer
 from addresses.models import AddressModel
 
 
 class AddressModelSerializer(BaseCustomModelSerializer):
+    parent_addr_title = serializers.CharField(source='parent_addr.title', read_only=True)
+
     class Meta:
         model = AddressModel
         fields = '__all__'
