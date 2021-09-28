7801ef5c26f85515a69db9c51f930c47205dd28f
Dmitry Novikov <nerosketch@gmail.com>
2021-09-28 11:31:48 +0300

    mk parent

--- apps/addresses/views.py
+++ apps/addresses/views.py
@@ -12,6 +12,12 @@ class AddressModelViewSet(DjingModelViewSet):
     serializer_class = AddressModelSerializer
     filterset_fields = ['address_type', 'parent_addr']
 
+    def filter_queryset(self, queryset):
+        parent_addr = safe_int(self.request.query_params.get('parent_addr'), default=None)
+        if parent_addr == 0:
+            return queryset.filter(parent_addr=None)
+        return super().filter_queryset(queryset)
+
     @action(methods=['get'], detail=False)
     def get_addr_types(self, request):
         types = [{'value': value, 'label': label} for value, label in AddressModelTypes.choices]
