2a10e732ebfc6d8d33591ae30b47b83de97b4390
Dmitry Novikov <nerosketch@gmail.com>
2021-10-27 10:22:44 +0300

    show customer bums

--- apps/customers/views/admin_side.py
+++ apps/customers/views/admin_side.py
@@ -380,6 +380,19 @@ class CustomerModelViewSet(SitesFilterMixin, DjingModelViewSet):
             'results': res_afk
         })
 
+    @action(methods=['get'], detail=False)
+    def bums(self, request):
+        qs = self.get_queryset().filter(address=None)
+        queryset = self.filter_queryset(qs)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class InvoiceForPaymentModelViewSet(DjingModelViewSet):
     queryset = models.InvoiceForPayment.objects.select_related("customer", "author")
