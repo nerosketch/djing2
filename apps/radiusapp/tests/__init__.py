8ae1ea5cf89384f218a0b723614665b521170dfa
Dmitry Novikov <nerosketch@gmail.com>
2022-01-11 16:25:30 +0300

    removed stale test cases

--- apps/radiusapp/tests/__init__.py
+++ apps/radiusapp/tests/__init__.py
@@ -1,7 +1,6 @@
 from .op82 import Option82TestCase
 from .build_dev_mac_by_opt82 import VendorsBuildDevMacByOpt82TestCase
 from .customer_auth import CustomerAuthTestCase
-# from .create_or_update_radius_session import CreateOrUpdateRadiusSession
 
 __all__ = (
     "Option82TestCase",
