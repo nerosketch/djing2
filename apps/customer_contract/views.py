a78583754128135a39170cc9f02d6ca7b208825f
Dmitry Novikov <nerosketch@gmail.com>
2022-02-17 11:39:13 +0300

    added finish action

--- apps/customer_contract/views.py
+++ apps/customer_contract/views.py
@@ -1,12 +1,20 @@
 from customer_contract import models
 from customer_contract import serializers
 from djing2.viewsets import DjingModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class CustomerContractModelViewSet(DjingModelViewSet):
     queryset = models.CustomerContractModel.objects.all()
     serializer_class = serializers.CustomerContractModelSerializer
     filterset_fields = ('customer',)
+
+    @action(methods=['put'], detail=True)
+    def finish(self, request, pk=None):
+        contract = self.get_object()
+        contract.finish()
+        return Response('ok')
 
 
 class CustomerContractAttachmentModelViewSet(DjingModelViewSet):
