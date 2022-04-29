7aecddc23f21b299693905a94affd8bfcf4f141e
Dmitry Novikov <nerosketch@gmail.com>
2022-04-29 16:21:19 +0300

    added locks to radius tasks

--- apps/networks/tasks.py
+++ apps/networks/tasks.py
@@ -1,10 +1,24 @@
 from datetime import datetime, timedelta
 from functools import wraps
-from uwsgi_tasks import task, cron, TaskExecutor, SPOOL_OK, SPOOL_RETRY
+from uwsgi_tasks import task, cron, TaskExecutor
 
 from djing2.lib.logger import logger
 from networks import radius_commands as rc
 from networks.models import CustomerIpLeaseModel
+
+
+def log(*args):
+    with open('/tmp/mule.log', 'a') as f:
+        f.write("%s\n" % '-'.join(str(i) for i in args))
+
+
+try:
+    import uwsgi
+except ImportError:
+    _stub_fn = lambda *_: ...
+    class uwsgi:
+        lock = _stub_fn
+        unlock = _stub_fn
 
 
 @cron(minute=-30)
@@ -12,34 +26,38 @@ def periodically_checks_for_stale_leases(signal_number):
     CustomerIpLeaseModel.objects.filter(last_update__lte=datetime.now() - timedelta(days=2), is_dynamic=True).delete()
 
 
-def _radius_task_error_wrapper(fn):
+def _radius_task_wrapper(fn):
     @wraps(fn)
     def _wrapped(*args, **kwargs):
         try:
+            uwsgi.lock()
+            log('UWSGI LOCK')
             return fn(*args, **kwargs)
-        except rc.RadiusSessionNotFoundException:
-            return SPOOL_OK
-        except rc.RadiusTimeoutException:
-            return SPOOL_RETRY
-        except rc.RadiusInvalidRequestException:
+        except rc.RadiusSessionNotFoundException as err:
+            logger.error('Radius session not found: %s' % str(err))
+        except rc.RadiusTimeoutException as err:
+            logger.error('Radius timeout: %s' % str(err))
+        except rc.RadiusInvalidRequestException as err:
             # may raised when trying to change to already installed service
-            return SPOOL_OK
-        # except rc.RadiusMissingAttributeException:
-        #    pass
+            logger.error('Radius invalid request: %s' % str(err))
+        except rc.RadiusMissingAttributeException as err:
+            logger.error('Radius missing attribute: %s' % str(err))
+        finally:
+            log('UWSGI UNLOCK')
+            uwsgi.unlock()
     return _wrapped
 
 
-@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=5)
-@_radius_task_error_wrapper
+@task(executor=TaskExecutor.MULE)
+@_radius_task_wrapper
 def async_finish_session_task(radius_uname: str):
     ret_text = rc.finish_session(radius_uname=radius_uname)
     if ret_text is not None:
         logger.warning(ret_text)
-    return SPOOL_OK
 
 
-@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=15)
-@_radius_task_error_wrapper
+@task(executor=TaskExecutor.MULE)
+@_radius_task_wrapper
 def async_change_session_inet2guest(radius_uname: str):
     """
     Async COA RADIUS sessions inet->guest.
@@ -50,7 +68,6 @@ def async_change_session_inet2guest(radius_uname: str):
     ret_text = rc.change_session_inet2guest(radius_uname)
     if ret_text is not None:
         logger.warning(ret_text)
-    return SPOOL_OK
 
 
 #
@@ -63,10 +80,10 @@ def async_change_session_inet2guest(radius_uname: str):
 #     speed_out_burst: int
 # )
 #
-@task(executor=TaskExecutor.SPOOLER, spooler_return=True, retry_timeout=15)
-@_radius_task_error_wrapper
+@task(executor=TaskExecutor.MULE)
+@_radius_task_wrapper
 def async_change_session_guest2inet(*args, **kwargs):
     ret_text = rc.change_session_guest2inet(*args, **kwargs)
     if ret_text is not None:
         logger.warning(ret_text)
-    return SPOOL_OK
+
