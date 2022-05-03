25403803c8b9f1a3702c6b497b82721b799e40fd
Dmitry Novikov <nerosketch@gmail.com>
2022-05-03 13:56:34 +0300

    moved radius views to one file

--- apps/radiusapp/urls.py
+++ apps/radiusapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import customers
+from radiusapp.views import RadiusCustomerServiceRequestViewSet
 
 app_name = "radiusapp"
 
 router = DefaultRouter()
 
 # customer radius views
-router.register("customer", customers.RadiusCustomerServiceRequestViewSet, basename="customer")
+router.register("customer", RadiusCustomerServiceRequestViewSet, basename="customer")
 
 urlpatterns = [path("", include(router.urls))]
