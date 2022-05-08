4ec782d919cf62bcf82321e746797d53048915e2
Dmitry Novikov <nerosketch@gmail.com>
2022-05-08 17:23:28 +0300

    fix

--- apps/customers/urls.py
+++ apps/customers/urls.py
@@ -23,11 +23,11 @@ router.register("additional-telephone", admin_side.AdditionalTelephoneModelViewS
 router.register("periodic-pay", admin_side.PeriodicPayForIdModelViewSet)
 router.register("attachments", admin_side.CustomerAttachmentViewSet)
 router.register("dynamic-fields", admin_side.CustomerDynamicFieldContentModelViewSet)
-router.register("customer-token", admin_side.SuperUserGetCustomerTokenByPhoneAPIView)
 router.register("", admin_side.CustomerModelViewSet)
 
 urlpatterns = [
     path("attach_group_service/", admin_side.AttachServicesToGroups.as_view()),
     path("groups_with_customers/", admin_side.groups_with_customers),
+    path("customer-token", admin_side.SuperUserGetCustomerTokenByPhoneAPIView.as_view()),
     path("", include(router.urls)),
 ]
