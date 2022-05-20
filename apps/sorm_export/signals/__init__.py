192b9d2bce438b4defd23a7706338993004f37fb
Dmitry Novikov <nerosketch@gmail.com>
2022-05-20 18:35:36 +0300

    legal branch constraint. Legal contract start date must be equal each branch contract date

--- apps/sorm_export/signals/__init__.py
+++ apps/sorm_export/signals/__init__.py
@@ -1,5 +1,6 @@
 from .aaa import *
 from .customer import *
+from .customer_legal import customer_branches_pre_save_signal
 from .device import *
 from .ip_numbering import *
 from .networks import *
---
+++ apps/sorm_export/signals/customer_legal.py
@@ -0,0 +1,21 @@
+from typing import Set
+from django.db.models.signals import m2m_changed
+from django.dispatch.dispatcher import receiver
+from djing2.lib import LogicError
+from customers_legal.models import CustomerLegalModel
+
+
+@receiver(m2m_changed, sender=CustomerLegalModel.branches.through)
+def customer_branches_pre_save_signal(sender, instance: CustomerLegalModel, action: str, model, pk_set: Set[int], **kwargs):
+    if action != 'pre_add':
+        return
+
+    legal_contract_date = instance.actual_start_time
+    new_customers = model.objects.filter(pk__in=pk_set)
+    for new_customer in new_customers:
+        for contract in new_customer.customercontractmodel_set.all():
+            if contract.start_service_time != legal_contract_date:
+                raise LogicError('Дата заключения договора филиала(абонента) ЮЛ не совпадает с датой начала договора ЮЛ. '
+                        'Это соответствие обязательно для СОРМ. Филиал(абонент): "%s", договор: "%s", ЮЛ: "%s"' % (
+                    new_customer.get_full_name(), contract.title, instance.title
+                ))
