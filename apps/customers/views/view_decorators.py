a8c43ddf8248a7bec12df26a664ee5410ff761d2
Dmitry Novikov <nerosketch@gmail.com>
2022-05-23 13:14:38 +0300

    ref

--- apps/customers/views/view_decorators.py
+++ apps/customers/views/view_decorators.py
@@ -1,8 +1,6 @@
 from rest_framework import status
 from rest_framework.response import Response
 
-from djing2.lib import LogicError
-
 # from gateways.nas_managers import GatewayFailedResult, GatewayNetworkError
 
 
@@ -10,8 +8,6 @@ def catch_customers_errs(fn):
     def wrapper(self, *args, **kwargs):
         try:
             return fn(self, *args, **kwargs)
-        except LogicError as e:
-            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
         except TimeoutError as e:
             return Response(str(e), status=status.HTTP_408_REQUEST_TIMEOUT)
 
