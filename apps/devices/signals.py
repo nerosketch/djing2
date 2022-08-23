b7d95089ba8490620f28a755fedffc9f6f41393e
Dmitry Novikov <nerosketch@gmail.com>
2022-08-23 11:25:02 +0300

    moved dev task to celery

--- apps/devices/signals.py
+++ apps/devices/signals.py
@@ -2,9 +2,9 @@ from django.db.models.signals import post_delete
 from django.dispatch.dispatcher import receiver
 
 from .models import Device
-from .tasks import unregister_device_async
+from .tasks import unregister_device_task
 
 
 @receiver(post_delete, sender=Device)
 def post_delete_device(sender, instance, **kwargs):
-    unregister_device_async(instance.pk)
+    unregister_device_task.delay(instance.pk)
