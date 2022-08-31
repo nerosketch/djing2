87b4325609adbc0866acb91e0450c073adf372e8
Dmitry Novikov <nerosketch@gmail.com>
2022-08-31 11:35:52 +0300

    maked computed fields

--- apps/djing2/lib/fastapi/crud.py
+++ apps/djing2/lib/fastapi/crud.py
@@ -1,5 +1,7 @@
 from typing import Type, Optional, List, Union, Any, Dict, OrderedDict as OrderedDictType
 from collections import OrderedDict
+
+from django.core.exceptions import FieldDoesNotExist
 from django.db.models import QuerySet, Model
 from django.db.utils import IntegrityError
 from djing2.lib.fastapi._types import IListResponse
@@ -12,6 +14,7 @@ from ._types import DEPENDENCIES, PAGINATION, PYDANTIC_SCHEMA as SCHEMA
 class DjangoCrudRouter(CRUDGenerator[SCHEMA]):
     _queryset: QuerySet
     _field_objects: OrderedDictType
+    _computed_field_objects: OrderedDictType
 
     def __init__(
         self,
@@ -31,9 +34,18 @@ class DjangoCrudRouter(CRUDGenerator[SCHEMA]):
     ) -> None:
         schema_fields = schema.__fields__
         model = queryset.model
-        field_objects = OrderedDict((fname, model._meta.get_field(fname)) for fname, _ in schema_fields.items())
-        self._field_objects = field_objects
         self._queryset = queryset
+
+        # Build model fields and computed fields
+        field_objects = {}
+        computed_field_objects = {}
+        for fname, schema_field in schema_fields.items():
+            try:
+                field_objects[fname] = model._meta.get_field(fname)
+            except FieldDoesNotExist:
+                computed_field_objects[fname] = schema_field
+        self._field_objects = OrderedDict(field_objects)
+        self._computed_field_objects = OrderedDict(computed_field_objects)
 
         super().__init__(
             schema=schema,
@@ -69,13 +81,18 @@ class DjangoCrudRouter(CRUDGenerator[SCHEMA]):
         return qs
 
     def format_object(self, model_item: Model, fields_list: Optional[List[str]] = None) -> OrderedDictType:
-        if fields_list is None:
-            return OrderedDict(
-                (fname, fobject.value_from_object(model_item)) for fname, fobject in self._field_objects.items()
-            )
-        return OrderedDict(
-            (fname, fobject.value_from_object(model_item)) for fname, fobject in self._field_objects.items() if fname in fields_list
+        object_fields = (
+            (fname, fobject.value_from_object(model_item)) for fname, fobject in self._field_objects.items()
         )
+        if fields_list is not None:
+            if not isinstance(fields_list, (list, tuple, set)):
+                raise ValueError('fields_list must be list, tuple or set: %s' % fields_list)
+            object_fields = ((fname, obj) for fname, obj in object_fields if fields_list)
+        r = OrderedDict(object_fields)
+        r.update({
+            fname: getattr(model_item, fname, None) for fname, ob in self._computed_field_objects.items()
+        })
+        return r
 
     @staticmethod
     def get_next_url(r: Request, current_page: int, all_count: int, limit: int) -> Optional[str]:
@@ -111,8 +128,9 @@ class DjangoCrudRouter(CRUDGenerator[SCHEMA]):
                 fields_list = fields.split(',')
                 if len(fields_list) > 0:
                     param_fields_list = set(fields_list)
-                    model_fields_list = {fname for fname, fobject in self._field_objects.items()}
+                    model_fields_list = {field_name for field_name, _ in self._field_objects.items()}
                     fields_list = param_fields_list & model_fields_list
+                    qs = qs.only(*fields_list)
                     # TODO: use computed fields from DRF serializer
 
             qs = self.paginate(qs=qs, page=page, page_size=page_size)
