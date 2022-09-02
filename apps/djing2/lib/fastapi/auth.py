b50c4694d668d9faba444804cb0aafa41009227f
Dmitry Novikov <nerosketch@gmail.com>
2022-09-02 12:29:53 +0300

    new dependency that checks is user admin

--- apps/djing2/lib/fastapi/auth.py
+++ apps/djing2/lib/fastapi/auth.py
@@ -1,29 +1,26 @@
-from fastapi import HTTPException
+from typing import Tuple
+
+from fastapi import HTTPException, Depends
 from fastapi.security import APIKeyHeader
+from profiles.models import BaseAccount
 from starlette import status
 from starlette.requests import Request
 from django.utils.translation import gettext as _
 from rest_framework.authtoken.models import Token
-from djing2.lib.auth_backends import _get_right_user
+from djing2.lib.auth_backends import get_right_user
+
+
+TOKEN_API_RESULT_TYPE = Tuple[BaseAccount, str]
 
 
 class TokenAPIKeyHeader(APIKeyHeader):
-    def __init__(self, auto_error: bool = True, *args, **kwargs):
-        super(TokenAPIKeyHeader, self).__init__(
-            auto_error=True,
-            *args, **kwargs
-        )
-
-    def __call__(self, request: Request):
+    def __call__(self, request: Request) -> TOKEN_API_RESULT_TYPE:
         token: str = request.headers.get(self.model.name)
         if not token:
-            if self.auto_error:
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail="Not authenticated"
-                )
-            else:
-                return None
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Not authenticated"
+            )
 
         divided_auth = token.split(' ')
         if not divided_auth or divided_auth[0].lower() != 'token':
@@ -46,17 +43,36 @@ class TokenAPIKeyHeader(APIKeyHeader):
         kw, token = divided_auth
 
         try:
-            token = Token.objects.select_related("user").get(key=token)
+            token_instance = Token.objects.select_related("user").get(key=token)
         except Token.DoesNotExist:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail=_("Invalid token.")
             ) from None
 
-        if not token.user.is_active:
+        if not token_instance.user.is_active:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail=_("User inactive or deleted.")
             )
 
-        return _get_right_user(token.user), token
+        user = get_right_user(token_instance.user)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Failed to get concrete profile'
+            )
+        return user, token
+
+
+token_auth_dep = TokenAPIKeyHeader(name='Authorization')
+
+
+def is_admin_auth_dependency(token_auth: TOKEN_API_RESULT_TYPE = Depends(token_auth_dep)) -> TOKEN_API_RESULT_TYPE:
+    user, token = token_auth
+    if not user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Forbidden'
+        )
+    return user, token
