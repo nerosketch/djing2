139645abb08a6f593b40685df291986e46240b11
Dmitry Novikov <nerosketch@gmail.com>
2022-09-06 12:28:31 +0300

    Maked contracts route in СОРМ

--- apps/sorm_export/views.py
+++ apps/sorm_export/views.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 from django.db.models import Count
 from djing2.lib.fastapi.crud import CRUDReadGenerator
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from djing2.lib.mixins import SitesFilterMixin
@@ -23,60 +23,72 @@ class CustomerModelSchemaWLegals(customers_schemas.CustomerModelSchema):
     legals: int
 
 
-_queryset = Customer.objects.annotate(
-    legals=Count('customerlegalmodel')
-).filter(
-    passportinfo=None,
-    legals=0,
-    is_active=True
-)
-
 router.include_router(CRUDReadGenerator(
     schema=CustomerModelSchemaWLegals,
     prefix='/passports',
-    queryset=_queryset
+    queryset=Customer.objects.annotate(
+            legals=Count('customerlegalmodel')
+        ).filter(
+            passportinfo=None,
+            legals=0,
+            is_active=True
+        )
 ))
 
 
-class SormCustomersWithoutContractsView(SitesFilterMixin, ReadOnlyModelViewSet):
-    """
-    Fetch example:
+class ContractsReadCRUD(CRUDReadGenerator):
+    def _get_all(self, *args, **kwargs):
+        ofn = super()._get_all(*args, **kwargs)
 
-    >>> import csv
-    >>> import requests
-    >>>
-    >>>
-    >>> r = requests.get('http://localhost:8000/api/sorm/', headers={
-    >>>     'Authorization': 'Token ffffffffffffffffffffffffffffffffffff',
-    >>>     'Content-type': 'application/json'
-    >>> }, params={
-    >>>     'fields': 'id,username,full_name'
-    >>> })
-    >>>
-    >>> customers = r.json()
-    >>>
-    >>> with open('customers_without_contracts.csv', 'w') as f:
-    >>>     writer = csv.DictWriter(f, fieldnames=['id', 'логин', 'фио'])
-    >>>     writer.writeheader()
-    >>>     for customer in customers:
-    >>>         vals = {
-    >>>             'id': customer.get('id'),
-    >>>             'логин': customer.get('username'),
-    >>>             'фио': customer.get('full_name')
-    >>>         }
-    >>>         writer.writerow(vals)
-    >>>
-    """
-    queryset = Customer.objects.annotate(
-        ccc=Count('customercontractmodel'),
-        legc=Count('customerlegalmodel')
-    ).filter(
-        ccc=0,
-        legc=0,
-        is_active=True
-    )
-    serializer_class = CustomerModelSerializer
-    permission_classes = [IsAuthenticated, IsAdminUser]
+        def _rar(item_id: int, request: Request):
+            """
+            Fetch example:
+
+            >>> import csv
+            >>> import requests
+            >>>
+            >>>
+            >>> r = requests.get('http://localhost:8000/api/sorm/contracts/', headers={
+            >>>     'Authorization': 'Token ffffffffffffffffffffffffffffffffffff',
+            >>>     'Content-type': 'application/json'
+            >>> }, params={
+            >>>     'fields': 'id,username,full_namevm ap'
+            >>> })
+            >>>
+            >>> customers = r.json()
+            >>>
+            >>> with open('customers_without_contracts.csv', 'w') as f:
+            >>>     writer = csv.DictWriter(f, fieldnames=['id', 'логин', 'фио'])
+            >>>     writer.writeheader()
+            >>>     for customer in customers:
+            >>>         vals = {
+            >>>             'id': customer.get('id'),
+            >>>             'логин': customer.get('username'),
+            >>>             'фио': customer.get('full_name')
+            >>>         }
+            >>>         writer.writerow(vals)
+            >>>
+            """
+            return ofn(item_id, request)
+
+        return _rar
+
+    # def _get_one(self, *args: Any, **kwargs: Any):
+    #     pass
+
+
+router.include_router(ContractsReadCRUD(
+    schema=customers_schemas.CustomerModelSchema,
+    prefix='/contracts',
+    queryset=Customer.objects.annotate(
+            ccc=Count('customercontractmodel'),
+            legc=Count('customerlegalmodel')
+        ).filter(
+            ccc=0,
+            legc=0,
+            is_active=True
+        ),
+))
 
 
 class SormCustomersTooOldView(SitesFilterMixin, ReadOnlyModelViewSet):
