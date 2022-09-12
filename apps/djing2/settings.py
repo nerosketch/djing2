adc5022ed5d6767a9ecd8b203f7f587c1cc05e74
Dmitry Novikov <nerosketch@gmail.com>
2022-09-12 17:13:44 +0300

    no use templates

--- apps/djing2/settings.py
+++ apps/djing2/settings.py
@@ -122,19 +122,19 @@ MIDDLEWARE = [
 ROOT_URLCONF = "djing2.urls"
 
 TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                # "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
+    # {
+    #     "BACKEND": "django.template.backends.django.DjangoTemplates",
+    #     # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
+    #     "APP_DIRS": True,
+    #     "OPTIONS": {
+    #         "context_processors": [
+    #             # "django.template.context_processors.debug",
+    #             "django.template.context_processors.request",
+    #             "django.contrib.auth.context_processors.auth",
+    #             "django.contrib.messages.context_processors.messages",
+    #         ],
+    #     },
+    # },
 ]
 
 AUTHENTICATION_BACKENDS = (
