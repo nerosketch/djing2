c403ebe960028663a9d8c465bb1b12cdd07302d1
Dmitry Novikov <nerosketch@gmail.com>
2022-09-13 11:50:52 +0300

    added permission check dependency

--- apps/customer_comments/views.py
+++ apps/customer_comments/views.py
@@ -1,6 +1,7 @@
 from django.utils.translation import gettext as _
 from djing2.lib.fastapi.auth import is_admin_auth_dependency
 from djing2.lib.fastapi.crud import NOT_FOUND
+from djing2.lib.fastapi.perms import permission_check_dependency
 from fastapi import APIRouter, Depends
 from starlette import status
 from customer_comments.models import CustomerCommentModel
@@ -18,9 +19,9 @@ router = APIRouter(
 
 
 @router.get('/', response_model=list[CustomerCommentModelSchema])
-# TODO: check rights for actions
-def get_all_comments(customer: int, is_auth=Depends(is_admin_auth_dependency)):
-    user, token = is_auth
+def get_all_comments(customer: int, user=Depends(permission_check_dependency(
+    perm_codename='customer_comments.view_customercommentmodel'
+))):
     qs = CustomerCommentModel.objects.filter(
         customer_id=customer,
     ).select_related("customer", "author").order_by("-id")
@@ -34,18 +35,20 @@ def get_all_comments(customer: int, is_auth=Depends(is_admin_auth_dependency)):
 
 
 @router.post('/', response_model=CustomerCommentModelSchema, status_code=status.HTTP_201_CREATED)
-def create_customer_comment(comment: CustomerCommentModelBaseSchema, is_auth=Depends(is_admin_auth_dependency)):
-    user, token = is_auth
+def create_customer_comment(comment: CustomerCommentModelBaseSchema, user=Depends(
+    permission_check_dependency(perm_codename='customer_comments.add_customercommentmodel')
+)):
     new_comment = CustomerCommentModel.objects.create(**comment.dict(), author_id=user.pk)
     new_comment.can_remove = user.pk == new_comment.author_id
     return CustomerCommentModelSchema.from_orm(new_comment)
 
 
 @router.delete('/{comment_id}/', status_code=status.HTTP_204_NO_CONTENT)
-def del_customer_comment(comment_id: int, is_auth=Depends(is_admin_auth_dependency)):
+def del_customer_comment(comment_id: int, user=Depends(
+    permission_check_dependency(perm_codename='customer_comments.delete_customercommentmodel')
+)):
     try:
         comment = CustomerCommentModel.objects.get(pk=comment_id)
-        user, token = is_auth
         if comment.author_id == user.pk:
             comment.delete()
             return Response(status_code=status.HTTP_204_NO_CONTENT)
---
+++ apps/djing2/lib/fastapi/perms.py
@@ -0,0 +1,21 @@
+from fastapi import Depends, HTTPException
+from profiles.models import BaseAccount
+from starlette import status
+from .auth import is_admin_auth_dependency
+
+
+PERMISSION_DENIED = HTTPException(
+    status_code=status.HTTP_403_FORBIDDEN,
+    detail='Permission denied, check your rights.'
+)
+
+
+def permission_check_dependency(perm_codename: str):
+    def _permission_check_dep(is_auth=Depends(is_admin_auth_dependency)) -> BaseAccount:
+        user, token = is_auth
+        has_perm = user.has_perm(perm=perm_codename)
+        if not has_perm:
+            raise PERMISSION_DENIED
+        return user
+
+    return _permission_check_dep
