146d998f48dde501b08228e21c086b3acf35e61f
Dmitry Novikov <nerosketch@gmail.com>
2022-09-14 15:14:43 +0300

    prettify get_all_address_duplicates route

--- apps/customers_duplicates/views.py
+++ apps/customers_duplicates/views.py
@@ -16,19 +16,20 @@ router = APIRouter(
 class DuplicateResult(BaseModel):
     customer_count: int
     address: int
+    address_full_title: str
     address_title: str
     customer_ids: list[int]
 
 
 @router.get('/', response_model=list[DuplicateResult])
-def get_all_address_duplicates():
+def get_all_address_duplicates(minimum_duplications: int = 2):
     """Get all customers with same addresses"""
-    qs = Customer.objects.values('address').annotate(
+    qs = Customer.objects.values('address', 'address__title').annotate(
         customer_count=Count('address'),
         customer_ids=ArrayAgg('pk')
-    ).filter(customer_count__gt=1)
+    ).filter(customer_count__gte=minimum_duplications)
     for c in qs.iterator():
         addr_title = AddressModel.objects.get_address_full_title(
             c['address']
         )
-        yield DuplicateResult(**c, address_title=addr_title)
+        yield DuplicateResult(**c, address_full_title=addr_title, address_title=c['address__title'])
