55dbb68ce27ce571f22a8475a9c5adb0d144fb42
Dmitry Novikov <nerosketch@gmail.com>
2022-10-19 15:02:46 +0300

    m

--- apps/customers/schemas.py
+++ apps/customers/schemas.py
@@ -78,7 +78,7 @@ class CustomerLogModelSchema(BaseModel):
     comment: str
     date: datetime
     author_name: Optional[str] = None
-    cost: float = Field(0.0, lt=2**64)
+    cost: float = 0.0
     from_balance: float = 0.0
     to_balance: float = 0.0
 
@@ -111,7 +111,7 @@ class InvoiceForPaymentModelSchema(BaseModel):
     author_id: int
     author_name: Optional[str] = None
     author_uname: Optional[str] = None
-    cost: Decimal = Field(lt=2**64)
+    cost: Decimal
 
     Config = OrmConf
 
@@ -250,7 +250,7 @@ class TypicalResponse(BaseModel):
 
 
 class AddBalanceRequestSchema(BaseModel):
-    cost: float = Field(lt=2**64)
+    cost: float
     comment: Optional[str] = Field(None, max_length=128)
 
     @validator('cost')
