c605ebd3611594e46b05e661a89d5e5aa1cf9b16
Dmitry Novikov <nerosketch@gmail.com>
2022-10-19 15:07:18 +0300

    Fixed response codes

--- apps/customers/tests/customer.py
+++ apps/customers/tests/customer.py
@@ -107,7 +107,7 @@ class CustomerModelAPITestCase(CustomAPITestCase):
     def test_stop_not_exists_service(self):
         models.Customer.objects.filter(username="custo1").update(current_service=None)
         r = self.get("/api/customers/%d/stop_service/" % self.customer.pk)
-        self.assertEqual(r.data, _("Service not connected"))
+        self.assertEqual(r.text, _("Service not connected"))
         self.assertEqual(r.status_code, status.HTTP_200_OK)
 
     def test_pick_admin_service_by_customer(self):
@@ -147,11 +147,11 @@ class CustomerModelAPITestCase(CustomAPITestCase):
 
     def test_add_balance_zero(self):
         r = self.post("/api/customers/%d/add_balance/" % self.customer.pk, {"cost": 0})
-        self.assertEqual(r.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(r.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
     def test_add_balance_text_invalid(self):
         r = self.post("/api/customers/%d/add_balance/" % self.customer.pk, {"cost": "sadasd"})
-        self.assertEqual(r.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(r.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
     def test_add_balance_ok(self):
         r = self.post("/api/customers/%d/add_balance/" % self.customer.pk, {"cost": 523})
@@ -163,7 +163,7 @@ class CustomerModelAPITestCase(CustomAPITestCase):
 
     def test_add_balance_long_comment(self):
         r = self.post("/api/customers/%d/add_balance/" % self.customer.pk, {"cost": 0xFF, "comment": "text " * 0xFFF})
-        self.assertEqual(r.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(r.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
 
     def test_add_balance_comment_bin(self):
         r = self.c.post(
