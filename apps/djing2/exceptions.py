60c12e2aa6609b51184aa1be12cc2ee8f9c9b173
Dmitry Novikov <nerosketch@gmail.com>
2022-11-16 13:34:45 +0300

    fixed legal branches

--- apps/djing2/exceptions.py
+++ apps/djing2/exceptions.py
@@ -1,9 +1,6 @@
-from djing2.lib import LogicError
 from fastapi import HTTPException
 from starlette import status
 from rest_framework.exceptions import APIException
-from rest_framework.views import exception_handler
-from rest_framework.response import Response
 
 
 class UniqueConstraintIntegrityError(APIException):
@@ -21,16 +18,4 @@ class ModelValidationError(HTTPException):
         super(ModelValidationError, self).__init__(
             status_code=status_code,
             detail=detail
-        )
-
-
-# TODO: deprecated. Only for Django Rest Framework
-def catch_logic_error(exc, context):
-    try:
-        response = exception_handler(exc, context)
-        return response
-    except LogicError as err:
-        return Response(
-            data=str(err),
-            status=status.HTTP_400_BAD_REQUEST
         )
