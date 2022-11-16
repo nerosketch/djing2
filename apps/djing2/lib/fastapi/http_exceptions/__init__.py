d6141a3e496ca5bf2c5a7665ac1af09b9154b9db
Dmitry Novikov <nerosketch@gmail.com>
2022-11-16 11:48:05 +0300

    process LogicError exception

--- apps/djing2/lib/fastapi/http_exceptions/__init__.py
+++ apps/djing2/lib/fastapi/http_exceptions/__init__.py
@@ -1,8 +1,10 @@
 from django.http.response import Http404
-from .main import django_http_resp_404_handler
+from .main import django_http_resp_404_handler, catch_logic_error
+from djing2.lib import LogicError
 
 handler_pairs = (
     (django_http_resp_404_handler, Http404),
+    (catch_logic_error, LogicError),
 )
 
 __all__ = ['handler_pairs']
--- apps/djing2/lib/fastapi/http_exceptions/main.py
+++ apps/djing2/lib/fastapi/http_exceptions/main.py
@@ -1,10 +1,19 @@
 from django.http.response import Http404
+from djing2.lib import LogicError
 from fastapi import Request
 from fastapi.responses import JSONResponse
+from starlette import status
+
+
+def catch_logic_error(request: Request, exc: LogicError):
+    return JSONResponse(
+        content=str(exc),
+        status_code=status.HTTP_400_BAD_REQUEST
+    )
 
 
 def django_http_resp_404_handler(request: Request, exc: Http404):
     return JSONResponse(
         content=str(exc),
-        status_code=404
+        status_code=status.HTTP_404_NOT_FOUND
     )
