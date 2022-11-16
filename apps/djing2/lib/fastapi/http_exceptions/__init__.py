cae1b6f4409babd3e38d68dcabcf3c23643492c8
Dmitry Novikov <nerosketch@gmail.com>
2022-11-16 11:59:58 +0300

    catch Customer err

--- apps/djing2/lib/fastapi/http_exceptions/__init__.py
+++ apps/djing2/lib/fastapi/http_exceptions/__init__.py
@@ -1,10 +1,8 @@
 from django.http.response import Http404
-from .main import django_http_resp_404_handler, catch_logic_error
-from djing2.lib import LogicError
+from .main import django_http_resp_404_handler
 
 handler_pairs = (
     (django_http_resp_404_handler, Http404),
-    (catch_logic_error, LogicError),
 )
 
 __all__ = ['handler_pairs']
