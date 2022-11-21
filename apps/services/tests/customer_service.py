ea36544ff428f9b25b7a10c93b25f43489297ac8
Dmitry Novikov <nerosketch@gmail.com>
2022-11-21 12:56:08 +0300

    added test for next months

--- apps/services/tests/customer_service.py
+++ apps/services/tests/customer_service.py
@@ -11,18 +11,28 @@ from freezegun import freeze_time
 from customers.models import CustomerLog, Customer
 from customers.tests.customer import CustomAPITestCase
 from networks.models import CustomerIpLeaseModel
-from services.models import Service, CustomerService
+from services import models
 from services.custom_logic import SERVICE_CHOICE_DEFAULT
 
 
+def create_customer_service_queue(customer: Customer, service: models.Service, num=1):
+    return models.CustomerServiceConnectingQueueModel.objects.bulk_create([
+        models.CustomerServiceConnectingQueueModel(
+            customer=customer,
+            service=service,
+            number_queue=n
+        ) for n in range(1, num+1, 1)
+    ])
+
+
 class CustomerServiceAutoconnectTestCase(CustomAPITestCase):
-    cs: CustomerService
+    cs: models.CustomerService
 
     def setUp(self):
         super().setUp()
 
         # service for tests
-        self.service = Service.objects.create(
+        self.service = models.Service.objects.create(
             title="test service",
             speed_in=10.0,
             speed_out=10.0,
@@ -63,6 +73,12 @@ class CustomerServiceAutoconnectTestCase(CustomAPITestCase):
         self.lease = self.lease.first()
         self.assertIsNotNone(self.lease)
 
+        create_customer_service_queue(
+            customer=self.customer,
+            service=self.service,
+            num=3
+        )
+
     def _check_customer_service(self, customer_service):
         self.assertIsNotNone(customer_service)
         self.assertEqual(customer_service.service.speed_in, 10.0)
@@ -72,11 +88,12 @@ class CustomerServiceAutoconnectTestCase(CustomAPITestCase):
         self.assertEqual(customer_service.service.calc_type, SERVICE_CHOICE_DEFAULT)
 
     def _pick_service(self, deadline: datetime, service=None, start_time: Optional[datetime] = None):
-        customer_service = CustomerService.objects.create(
+        customer_service = models.CustomerService.objects.create(
             service=service or self.service,
             start_time=start_time or datetime.now(),
             deadline=deadline,
-            customer=self.customer
+            customer=self.customer,
+            cost=service.cost if service else self.service.cost
         )
         return customer_service
 
@@ -84,27 +101,27 @@ class CustomerServiceAutoconnectTestCase(CustomAPITestCase):
         customer = self.customer
 
         # before all, initial
-        curr_cust_srv = CustomerService.objects.filter(customer=customer).first()
+        curr_cust_srv = models.CustomerService.objects.filter(customer=customer).first()
         self.assertEqual(curr_cust_srv.service_id, self.service.pk)
         self.assertEqual(customer.balance, 10)
-        CustomerService.objects.continue_services_if_autoconnect(customer=customer)
+        models.CustomerService.objects.continue_services_if_autoconnect(customer=customer)
 
         # after first try, when time not expired
         self.assertEqual(customer.balance, 10)
-        curr_cust_srv = CustomerService.objects.filter(customer=customer).first()
+        curr_cust_srv = models.CustomerService.objects.filter(customer=customer).first()
         self.assertEqual(curr_cust_srv.service_id, self.service.pk)
 
         # decrease time
-        CustomerService.objects.filter(pk=self.cs.pk).update(
+        models.CustomerService.objects.filter(pk=self.cs.pk).update(
             start_time=F('start_time') - timedelta(minutes=1),
             deadline=F('deadline') - timedelta(minutes=1)
         )
 
         # now time is expired, continue service
-        CustomerService.objects.continue_services_if_autoconnect(customer=customer)
+        models.CustomerService.objects.continue_services_if_autoconnect(customer=customer)
         customer.refresh_from_db()
         self.assertEqual(customer.balance, 8)
-        curr_cust_srv = CustomerService.objects.filter(customer=customer).first()
+        curr_cust_srv = models.CustomerService.objects.filter(customer=customer).first()
         self.assertEqual(curr_cust_srv.service_id, self.service.pk)
 
         logs = CustomerLog.objects.filter(
@@ -117,33 +134,33 @@ class CustomerServiceAutoconnectTestCase(CustomAPITestCase):
         self.assertEqual(logs.count(), 1, msg=logs)
 
     def test_get_user_credentials_by_ip(self):
-        customer_service = CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.2")
+        customer_service = models.CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.2")
         self._check_customer_service(customer_service)
 
     def test_not_exists_lease(self):
-        customer_service = CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.12")
+        customer_service = models.CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.12")
         self.assertIsNone(customer_service)
 
     def test_if_ip_is_none(self):
-        customer_service = CustomerService.get_user_credentials_by_ip(ip_addr=None)
+        customer_service = models.CustomerService.get_user_credentials_by_ip(ip_addr=None)
         self.assertIsNone(customer_service)
 
     def test_if_ip_is_bad(self):
-        customer_service = CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.12.13")
+        customer_service = models.CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.12.13")
         self.assertIsNone(customer_service)
 
     def test_customer_disabled(self):
         self.customer.is_active = False
         self.customer.save(update_fields=["is_active"])
         # self.customer.refresh_from_db()
-        customer_service = CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.2")
+        customer_service = models.CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.2")
         self.assertIsNone(customer_service)
 
     def test_customer_does_not_have_service(self):
         self.cs.stop_service(
             author_profile=self.customer
         )
-        customer_service = CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.2")
+        customer_service = models.CustomerService.get_user_credentials_by_ip(ip_addr="10.11.12.2")
         self.assertIsNone(customer_service)
 
     def test_customer_with_onu_device(self):
@@ -176,7 +193,7 @@ class CustomerServiceAutoconnectTestCase(CustomAPITestCase):
         self.assertIsNotNone(self.lease)
         # lease must be contain ip_address=10.11.12.3'
 
-        customer_service = CustomerService.get_user_credentials_by_ip(
+        customer_service = models.CustomerService.get_user_credentials_by_ip(
             ip_addr="10.11.12.3",
         )
         self._check_customer_service(customer_service)
@@ -190,9 +207,9 @@ class CustomerServiceAutoconnectTestCase(CustomAPITestCase):
         self.assertEqual(r.status_code, status.HTTP_418_IM_A_TEAPOT)
 
     @freeze_time(datetime(year=2020, month=11, day=30))
-    def test_last_minute_autoconnect(self):
+    def test_continue_from_one_month_to_another(self):
         customer = self.customer
-        curr_cust_qs = CustomerService.objects.filter(customer=customer)
+        curr_cust_qs = models.CustomerService.objects.filter(customer=customer)
 
         # set times
         curr_cust_qs.update(
@@ -208,7 +225,7 @@ class CustomerServiceAutoconnectTestCase(CustomAPITestCase):
         # self.assertEqual(curr_srv.service_id, self.service.pk)
         # self.assertEqual(customer.balance, 10)
 
-        CustomerService.objects.continue_services_if_autoconnect(
+        models.CustomerService.objects.continue_services_if_autoconnect(
             customer=customer
         )
         curr_srv.refresh_from_db()
@@ -224,3 +241,53 @@ class CustomerServiceAutoconnectTestCase(CustomAPITestCase):
             year=2020, month=12, day=1
         )
         self.assertEqual(curr_srv.deadline, expected_deadline)
+
+    def _connect_service_and_check(self, customer, expected_start_time, expected_deadline):
+        models.connect_service_if_autoconnect(customer_id=customer.pk)
+        customer.refresh_from_db()
+
+        current_customer_srv = models.CustomerService.objects.get(customer=customer)
+        self.assertEqual(current_customer_srv.customer, customer)
+        self.assertEqual(current_customer_srv.service, self.service)
+        self.assertEqual(current_customer_srv.start_time, expected_start_time)
+        self.assertEqual(current_customer_srv.deadline, expected_deadline)
+        self.assertEqual(customer.balance, 8, msg=customer)
+
+    @freeze_time(datetime(year=2020, month=11, day=1, hour=3, minute=15, second=14))
+    def test_connect_from_one_month_to_another(self):
+        customer = self.customer
+        models.CustomerService.objects.filter(customer=customer).delete()
+
+        expected_start_time = datetime(
+            year=2020, month=11, day=1, hour=3, minute=15, second=14
+        )
+        expected_deadline = datetime(
+            year=2020, month=12, day=1
+        )
+        self._connect_service_and_check(
+            customer,
+            expected_start_time,
+            expected_deadline
+        )
+
+    @freeze_time(datetime(year=2020, month=12, day=1, hour=3, minute=15, second=14))
+    def test_multiple_continue(self):
+        customer = self.customer
+        models.CustomerService.objects.filter(customer=customer).delete()
+
+        expected_start_time = datetime(
+            year=2020, month=12, day=1, hour=3, minute=15, second=14
+        )
+        expected_deadline = datetime(
+            year=2021, month=1, day=1
+        )
+        self._connect_service_and_check(
+            customer,
+            expected_start_time,
+            expected_deadline
+        )
+        self._connect_service_and_check(
+            customer,
+            expected_start_time,
+            expected_deadline
+        )
