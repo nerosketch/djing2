5c23a28a2b15d356f60d41f60358e5e6587c8b0e
Dmitry Novikov <nerosketch@gmail.com>
2022-11-23 16:24:34 +0300

    signal that updates service queue during connectiong new service

--- apps/services/signals.py
+++ apps/services/signals.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from django.db.models.signals import post_init, pre_save
 from django.dispatch.dispatcher import receiver
-from services.models import CustomerService
+from services.custom_signals import customer_service_post_pick
+from services.models import CustomerService, CustomerServiceConnectingQueueModel
 
 
 @receiver(post_init, sender=CustomerService)
@@ -18,3 +19,12 @@ def customer_service_pre_save(sender, instance: CustomerService, **kwargs):
     customer_service = instance
     if getattr(customer_service, "deadline") is None:
         customer_service.assign_deadline()
+
+
+@receiver(customer_service_post_pick, sender=CustomerService)
+def on_connect_new_service_update_first_item_in_queue(sender, instance: CustomerService, **kwargs):
+    CustomerServiceConnectingQueueModel.objects.filter(
+        customer_id=instance.customer_id
+    ).replace_first(
+        service_id=instance.service_id
+    )
