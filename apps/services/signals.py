fc38f430b00e34dadfe72ec8612d7eda6b74ba04
Dmitry Novikov <nerosketch@gmail.com>
2022-11-28 10:36:18 +0300

    maked signal that checks customer service continuation possibility

--- apps/services/signals.py
+++ apps/services/signals.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from django.db.models.signals import post_init, pre_save
 from django.dispatch.dispatcher import receiver
+from fin_app.custom_signals import after_payment_success
+from customers.models import Customer
 from services.custom_signals import customer_service_post_pick
 from services.models import CustomerService, CustomerServiceConnectingQueueModel
+from services.tasks import customer_check_service_for_expiration_task
 
 
 @receiver(post_init, sender=CustomerService)
@@ -28,3 +31,8 @@ def on_connect_new_service_update_first_item_in_queue(sender, instance: Customer
     ).replace_first(
         service_id=instance.service_id
     )
+
+
+@receiver(after_payment_success, sender=Customer)
+def on_customer_pay_success_check_service_connection(sender, instance: Customer, **kwargs):
+    customer_check_service_for_expiration_task(customer_id=instance.pk)
