617a1b76d9794180420dd823920ba0e67f4938d6
Dmitry Novikov <nerosketch@gmail.com>
2022-11-28 16:50:47 +0300

    fixed perm

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -702,7 +702,7 @@ def remove_task(task_id: int,
         qs_or_model=models.Task,
         curr_user=curr_user,
         curr_site=curr_site,
-        perm_codename='customers.delete_customer'
+        perm_codename='tasks.delete_task'
     )
     # .annotate(
     #     recipients_agg=ArrayAgg('recipients')
