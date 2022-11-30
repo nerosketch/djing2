ac25cd7ea16ac731589a9be64bba406c5f375605
Dmitry Novikov <nerosketch@gmail.com>
2022-11-30 17:18:57 +0300

    refactoring services user routes

--- apps/services/views/__init__.py
+++ apps/services/views/__init__.py
@@ -3,6 +3,7 @@ from .customer_service import router as csr
 from .shot import router as shot_rt
 from .periodic import router as per_rt
 from .service_queue import router as queue_rt
+from .user_side import router as usr_rt
 from .admin_side import router as adm_rt
 
 router = APIRouter(
@@ -14,4 +15,5 @@ router.include_router(csr)
 router.include_router(shot_rt)
 router.include_router(per_rt)
 router.include_router(queue_rt)
+router.include_router(usr_rt)
 router.include_router(adm_rt)
