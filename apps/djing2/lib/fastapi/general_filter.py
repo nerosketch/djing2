920a8c79836366748e2b20c3e7ba6d4df3f93ae1
Dmitry Novikov <nerosketch@gmail.com>
2022-12-21 16:12:52 +0300

    mk base

--- apps/djing2/lib/fastapi/general_filter.py
+++ apps/djing2/lib/fastapi/general_filter.py
@@ -1,9 +1,11 @@
 from typing import Optional, Union, Type
 from django.db.models import QuerySet, Model
 from django.contrib.sites.models import Site
+from djing2.lib.fastapi.perms import permission_check_dependency
+from fastapi import Depends
 from profiles.models import BaseAccount
 from .perms import filter_qs_by_rights
-from .sites_depend import filter_qs_with_sites
+from .sites_depend import filter_qs_with_sites, sites_dependency
 
 
 def general_filter_queryset(qs_or_model: Union[QuerySet, Type[Model]], curr_user: BaseAccount,
@@ -20,3 +22,22 @@ def general_filter_queryset(qs_or_model: Union[QuerySet, Type[Model]], curr_user
         curr_site=curr_site
     )
     return rqs
+
+
+# TODO: Use it instead of general_filter_queryset in many routes
+def general_prepare_queryset_dependency(perm_codename, qs_or_model: Union[QuerySet, Type[Model]]):
+    def _w(
+        curr_site: Site = Depends(sites_dependency),
+        curr_user: BaseAccount = Depends(permission_check_dependency(
+            perm_codename=perm_codename
+        )),
+    ) -> QuerySet:
+        qs = general_filter_queryset(
+            qs_or_model=qs_or_model,
+            curr_site=curr_site,
+            curr_user=curr_user,
+            perm_codename=perm_codename
+        )
+        return qs
+
+    return _w
--- apps/fin_app/views/base.py
+++ apps/fin_app/views/base.py
@@ -1,7 +1,9 @@
-from django.db.models import Count
+from django.db.models import Count, QuerySet
+from django.db import transaction
 from djing2.lib.fastapi.auth import is_admin_auth_dependency
-from djing2.lib.fastapi.crud import CrudRouter, CRUDReadGenerator
-from fastapi import APIRouter, Depends
+from djing2.lib.fastapi.crud import CRUDReadGenerator
+from djing2.lib.fastapi.general_filter import general_prepare_queryset_dependency
+from fastapi import APIRouter, Depends, Path
 from fin_app.models.base_payment_model import (
     BasePaymentModel,
     BasePaymentLogModel,
@@ -25,14 +27,35 @@ def pays_report(params: schemas.PaysReportParamsSchema = Depends()):
     )
 
 
-router.include_router(CrudRouter(
+router.include_router(CRUDReadGenerator(
     schema=schemas.BasePaymentModelSchema,
-    update_schema=schemas.BasePaymentBaseSchema,
     queryset=BasePaymentModel.objects.order_by('id').annotate(
         pay_count=Count("basepaymentlogmodel")
     ),
-    create_route=False
 ))
+
+
+@router.patch('/{payment_id}/',
+              response_model=schemas.BasePaymentModelSchema)
+def update_base_payment_model(
+    payload: schemas.BasePaymentBaseSchema,
+    payment_id: int = Path(gt=0),
+    qs: QuerySet = Depends(general_prepare_queryset_dependency(
+        perm_codename='fin_app.view_basepaymentmodel',
+        qs_or_model=BasePaymentModel
+    ))
+):
+    qs = qs.filter(pk=payment_id)
+    sites = payload.sites
+    with transaction.atomic():
+        qs.update(**payload.dict(
+            exclude_unset=True,
+            exclude={'sites'}
+        ))
+        instance = qs.first()
+        if sites:
+            instance.sites.set(sites)
+    return schemas.BasePaymentModelSchema.from_orm(instance)
 
 
 router.include_router(CRUDReadGenerator(
