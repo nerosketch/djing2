b10b9f32701a61e4696f95042ba56d8b3f8376f3
Dmitry Novikov <nerosketch@gmail.com>
2022-12-23 22:17:48 +0300

    c

--- update_release.sh
+++ update_release.sh
@@ -4,7 +4,7 @@ set -e
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-dock=$(which docker)
+which docker
 if [ $? -eq 1 ]; then
   echo "docker not found. Check if it is installed."
   exit 1
